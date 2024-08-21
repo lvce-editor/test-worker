ce5d348235eff819ab77093c78f57a89b44bd629
Le Vivilet <levivlet@pm.me>
2024-08-21 19:16:57 +0200

    feature: send messageport to webview

--- src/parts/SendPortToWebView/SendPortToWebView.ts
+++ src/parts/SendPortToWebView/SendPortToWebView.ts
@@ -1,7 +1,10 @@
 import * as Rpc from '../Rpc/Rpc.ts'
 
 export const sendPortToWebView = async (webviewId: string, port: MessagePort) => {
+  // TODO use unique id instead of webview id
+  // TODO ask renderer worker for a qnique id?
   await Rpc.invokeAndTransfer([port], 'Transferrable.transferToRendererProcess', webviewId, port)
+  await Rpc.invoke('Transferrable.transferToWebView', webviewId)
   console.log('did send port to renderer process')
   // TODO ask renderer process to transfer the port to the webview
 }
