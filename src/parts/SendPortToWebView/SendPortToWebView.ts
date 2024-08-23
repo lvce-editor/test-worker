f70d79b9391ffa007aa7e3f1fe0e232e72a0ca82
Le Vivilet <levivlet@pm.me>
2024-08-23 22:36:04 +0200

    feature: automatically transfer jsonrpc transferrables

--- src/parts/SendPortToWebView/SendPortToWebView.ts
+++ src/parts/SendPortToWebView/SendPortToWebView.ts
@@ -1,7 +1,7 @@
 import * as Rpc from '../Rpc/Rpc.ts'
 
 export const sendPortToWebView = async (webviewId: string, port: MessagePort) => {
-  await Rpc.invokeAndTransfer([port], 'Transferrable.transferToRendererProcess', webviewId, port)
+  await Rpc.invokeAndTransfer('Transferrable.transferToRendererProcess', webviewId, port)
   console.log('did send port to renderer process')
   // TODO ask renderer process to transfer the port to the webview
 }
