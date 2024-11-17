0c8d428dc9babbd90cf63cab83bc4e21a0567915
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-17 13:46:52 +0100

    feature: use explciit return types (#53)

--- src/parts/TestFrameWorkComponentExtension/TestFrameWorkComponentExtension.ts
+++ src/parts/TestFrameWorkComponentExtension/TestFrameWorkComponentExtension.ts
@@ -1,11 +1,13 @@
 import * as Rpc from '../ParentRpc/ParentRpc.ts'
 
 export const addWebExtension = async (relativePath: string): Promise<void> => {
+  // TODO compute absolutePath
   const absolutePath = relativePath
   await Rpc.invoke('ExtensionMeta.addWebExtension', absolutePath)
 }
 
 export const addNodeExtension = async (relativePath: string): Promise<void> => {
+  // TODO compute absolutePath
   const absolutePath = relativePath
   await Rpc.invoke('ExtensionMeta.addNodeExtension', absolutePath)
 }
