7bb42553f22baf330c3782a4e39fbdda0f9de438
Le Vivilet <levivlet@pm.me>
2024-11-17 13:45:15 +0100

    feature: use explciit return types

--- src/parts/TestFrameWorkComponentExtension/TestFrameWorkComponentExtension.ts
+++ src/parts/TestFrameWorkComponentExtension/TestFrameWorkComponentExtension.ts
@@ -1,12 +1,12 @@
 import * as Rpc from '../ParentRpc/ParentRpc.ts'
 
-export const addWebExtension = async (relativePath: string) => {
+export const addWebExtension = async (relativePath: string): Promise<void> => {
   // TODO compute absolutePath
   const absolutePath = relativePath
   await Rpc.invoke('ExtensionMeta.addWebExtension', absolutePath)
 }
 
-export const addNodeExtension = async (relativePath: string) => {
+export const addNodeExtension = async (relativePath: string): Promise<void> => {
   // TODO compute absolutePath
   const absolutePath = relativePath
   await Rpc.invoke('ExtensionMeta.addNodeExtension', absolutePath)
--- src/parts/TestFrameWorkComponentSourceControl/TestFrameWorkComponentSourceControl.ts
+++ src/parts/TestFrameWorkComponentSourceControl/TestFrameWorkComponentSourceControl.ts
@@ -1,9 +1,9 @@
 import * as Rpc from '../ParentRpc/ParentRpc.ts'
 
-export const acceptInput = async () => {
+export const acceptInput = async (): Promise<void> => {
   await Rpc.invoke('Source Control.acceptInput')
 }
 
-export const handleInput = async (text: string) => {
+export const handleInput = async (text: string): Promise<void> => {
   await Rpc.invoke('Source Control.handleInput', text)
 }
