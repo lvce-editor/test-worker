ad967a91946634af715af70d4c9a96b7eed386e8
Le Vivilet <levivlet@pm.me>
2024-11-27 23:27:29 +0100

    feature: enable stricter eslint

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
   eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.strictTypeChecked,
   {
     languageOptions: {
       parserOptions: {
