43c816a95c36f1b3631473bfb342229da16b3f71
Le Vivilet <levivlet@pm.me>
2024-12-15 22:34:44 +0100

    feature: add more search commands

--- src/parts/TestFrameWorkComponentSearch/TestFrameWorkComponentSearch.ts
+++ src/parts/TestFrameWorkComponentSearch/TestFrameWorkComponentSearch.ts
@@ -3,3 +3,51 @@ import * as Rpc from '../ParentRpc/ParentRpc.ts'
 export const setValue = async (value: string) => {
   await Rpc.invoke('Search.handleInput', value)
 }
+
+export const clearSearchResults = async () => {
+  await Rpc.invoke('Search.clearSearchResults')
+}
+
+export const dismissItem = async () => {
+  await Rpc.invoke('Search.dismissItem')
+}
+
+export const focusFirst = async () => {
+  await Rpc.invoke('Search.focusFirst')
+}
+
+export const focusIndex = async (index: number) => {
+  await Rpc.invoke('Search.focusIndex', index)
+}
+
+export const focusNext = async () => {
+  await Rpc.invoke('Search.focusNext')
+}
+
+export const focusNextPage = async () => {
+  await Rpc.invoke('Search.focusPage')
+}
+
+export const focusPreviousPage = async () => {
+  await Rpc.invoke('Search.focusPreviousPage')
+}
+
+export const focusPrevious = async () => {
+  await Rpc.invoke('Search.focusPrevious')
+}
+
+export const toggleMatchCase = async () => {
+  await Rpc.invoke('Search.toggleMatchCase')
+}
+
+export const toggleMatchWholeWord = async () => {
+  await Rpc.invoke('Search.toggleMatchWholeWord')
+}
+
+export const togglePreserveCase = async () => {
+  await Rpc.invoke('Search.togglePreserveCase')
+}
+
+export const toggleUseRegularExpression = async () => {
+  await Rpc.invoke('Search.toggleUseRegularExpression')
+}
