f0653b6f01751a02bcdfe38c1697eeaa792d281b
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-15 22:25:28 +0100

    feature: add more about commands (#64)

--- src/parts/TestFrameWorkComponentAbout/TestFrameWorkComponentAbout.ts
+++ src/parts/TestFrameWorkComponentAbout/TestFrameWorkComponentAbout.ts
@@ -3,3 +3,15 @@ import * as Rpc from '../ParentRpc/ParentRpc.ts'
 export const show = async () => {
   return Rpc.invoke('About.showAbout')
 }
+
+export const handleClickOk = async () => {
+  return Rpc.invoke('About.handleClickOk')
+}
+
+export const handleClickClose = async () => {
+  return Rpc.invoke('About.handleClickClose')
+}
+
+export const handleClickCopy = async () => {
+  return Rpc.invoke('About.handleClickCopy')
+}
