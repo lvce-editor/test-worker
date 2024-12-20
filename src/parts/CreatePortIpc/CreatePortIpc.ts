6f87ab45de79fe4aa7d76d9143e44d322fb18da5
Le Vivilet <levivlet@pm.me>
2024-12-20 18:55:19 +0100

    feature: send messageport to webview correctly

--- src/parts/CreatePortIpc/CreatePortIpc.ts
+++ src/parts/CreatePortIpc/CreatePortIpc.ts
@@ -1,6 +1,6 @@
 import * as GetPortTuple from '../GetPortTuple/GetPortTuple.ts'
 import * as JsonRpc from '../JsonRpc/JsonRpc.ts'
-import * as SendPortToWebView from '../SendPortToWebView/SendPortToWebView.ts'
+import * as ParentRpc from '../ParentRpc/ParentRpc.ts'
 import * as WaitForReadyEvent from '../WaitForReadyEvent/WaitForReadyEvent.ts'
 
 const preparePrettyError = (error: any): any => {
@@ -20,7 +20,14 @@ const requiresSocket = (): boolean => {
 export const createPortIpc = async (webViewId: string): Promise<any> => {
   const { port1, port2 } = GetPortTuple.getPortTuple()
   const firstEventPromise = WaitForReadyEvent.waitForFirstEventEvent(port1)
-  await SendPortToWebView.sendPortToWebView(webViewId, port2)
+  // TODO ask extension host worker about webview uid
+
+  const info = await ParentRpc.invoke('WebView.getWebViewInfo', webViewId)
+
+  const portType = 'test'
+  await ParentRpc.invokeAndTransfer('WebView.setPort', info.uid, port2, info.origin, portType)
+
+  // await SendPortToWebView.sendPortToWebView(webViewId, port2)
   const firstEvent = await firstEventPromise
   if (firstEvent.data !== 'ready') {
     throw new Error('unexpected first message')
