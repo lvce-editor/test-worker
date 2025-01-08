74933d49b561331f17b5f4b98a3e1984a2b36fd3
Le Vivilet <levivlet@pm.me>
2025-01-08 22:07:51 +0100

    feature: use rpc dependency

--- src/parts/ParentRpc/ParentRpc.ts
+++ src/parts/ParentRpc/ParentRpc.ts
@@ -1,25 +1,12 @@
-import * as IpcState from '../IpcState/IpcState.ts'
-
-const state = {
-  rpc: undefined,
-}
+import * as RpcId from '../RpcId/RpcId.ts'
+import * as RpcRegistry from '../RpcRegistry/RpcRegistry.ts'
 
 export const invoke = (method: string, ...params: any[]): Promise<any> => {
-  const rpc = state.rpc
-  // @ts-ignore
+  const rpc = RpcRegistry.get(RpcId.RendererWorker)
   return rpc.invoke(method, ...params)
 }
 
 export const invokeAndTransfer = (method: string, ...params: any[]) => {
-  const rpc = state.rpc
-  // @ts-ignore
+  const rpc = RpcRegistry.get(RpcId.RendererWorker)
   return rpc.invokeAndTransfer(method, ...params)
-}
-
-export const listen = (ipc: any): void => {
-  IpcState.set(ipc)
-}
-
-export const setRpc = (rpc: any): void => {
-  state.rpc = rpc
 }
---
+++ src/parts/RpcId/RpcId.ts
@@ -0,0 +1,1 @@
+export const RendererWorker = 1
