bbb8b9f75b5d04a055a6212a7923a8054458d184
Le Vivilet <levivlet@pm.me>
2025-01-14 22:40:36 +0100

    lint

--- src/parts/SendPortToWebView/SendPortToWebView.ts
+++ src/parts/SendPortToWebView/SendPortToWebView.ts
@@ -1,5 +1,6 @@
 import * as Rpc from '../ParentRpc/ParentRpc.ts'
 
+// eslint-disable-next-line  @typescript-eslint/prefer-readonly-parameter-types
 export const sendPortToWebView = async (webviewId: string, port: MessagePort): Promise<void> => {
   await Rpc.invokeAndTransfer('Transferrable.transferToRendererProcess', webviewId, port)
   console.log('did send port to renderer process')
