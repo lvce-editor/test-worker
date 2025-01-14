ca23e5fa20bae2998574c352a5da297dea610c8b
Le Vivilet <levivlet@pm.me>
2025-01-14 22:39:54 +0100

    lint

--- src/parts/ParentRpc/ParentRpc.ts
+++ src/parts/ParentRpc/ParentRpc.ts
@@ -1,12 +1,12 @@
 import * as RpcId from '../RpcId/RpcId.ts'
 import * as RpcRegistry from '../RpcRegistry/RpcRegistry.ts'
 
-export const invoke = (method: string, ...params: any[]): Promise<any> => {
+export const invoke = (method: string, ...params: readonly any[]): Promise<any> => {
   const rpc = RpcRegistry.get(RpcId.RendererWorker)
   return rpc.invoke(method, ...params)
 }
 
-export const invokeAndTransfer = (method: string, ...params: any[]): Promise<any> => {
+export const invokeAndTransfer = (method: string, ...params: readonly any[]): Promise<any> => {
   const rpc = RpcRegistry.get(RpcId.RendererWorker)
   return rpc.invokeAndTransfer(method, ...params)
 }
