baa72f9b246466e1ac69f67368b2898f9bbc30b6
Le Vivilet <levivlet@pm.me>
2025-01-19 21:28:03 +0100

    feature: add selectFeature command

--- src/parts/TestFrameWorkComponentExtensionDetail/TestFrameWorkComponentExtensionDetail.ts
+++ src/parts/TestFrameWorkComponentExtensionDetail/TestFrameWorkComponentExtensionDetail.ts
@@ -1,5 +1,9 @@
 import * as Rpc from '../ParentRpc/ParentRpc.ts'
 
+export const selectFeature = (name: string): Promise<void> => {
+  return Rpc.invoke('ExtensionDetail.selectFeature', name)
+}
+
 export const selectTab = (name: string): Promise<void> => {
   return Rpc.invoke('ExtensionDetail.selectTab', name)
 }
