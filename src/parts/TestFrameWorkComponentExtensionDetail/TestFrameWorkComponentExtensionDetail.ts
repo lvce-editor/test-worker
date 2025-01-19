f2def496b43376f0ca7ee322fedc2628d12395c2
Le Vivilet <levivlet@pm.me>
2025-01-19 21:27:36 +0100

    feature: add extension detail open command

--- src/parts/TestFrameWorkComponentExtensionDetail/TestFrameWorkComponentExtensionDetail.ts
+++ src/parts/TestFrameWorkComponentExtensionDetail/TestFrameWorkComponentExtensionDetail.ts
@@ -3,3 +3,8 @@ import * as Rpc from '../ParentRpc/ParentRpc.ts'
 export const selectTab = (name: string): Promise<void> => {
   return Rpc.invoke('ExtensionDetail.selectTab', name)
 }
+
+export const open = (extensionId: string): Promise<void> => {
+  const uri = `extension-detail://${extensionId}`
+  return Rpc.invoke('Main.openUri', uri)
+}
