89e2a9761bc3be076f17b0e8665c6c38f90982e5
Le Vivilet <levivlet@pm.me>
2025-02-22 23:21:39 +0100

    feature: add webviewstate

---
+++ src/parts/WebViewState/WebViewState.ts
@@ -0,0 +1,9 @@
+const webViews = Object.create(null)
+
+export const set = (id: string, webView: any): void => {
+  webViews[id] = webView
+}
+
+export const get = (id: string) => {
+  return webViews[id]
+}
