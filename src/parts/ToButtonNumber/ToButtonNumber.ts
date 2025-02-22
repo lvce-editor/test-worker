742063dbcfd162d48e377e78a0f133c4b8169410
Le Vivilet <levivlet@pm.me>
2025-02-22 20:41:26 +0100

    feature: move toButtonNumber into separate file

---
+++ src/parts/ToButtonNumber/ToButtonNumber.ts
@@ -0,0 +1,14 @@
+
+
+export const toButtonNumber = (buttonType:string): number => {
+  switch (buttonType) {
+    case 'left':
+      return 0
+    case 'middle':
+      return 1
+    case 'right':
+      return 2
+    default:
+      throw new Error(`unsupported button type: ${buttonType}`)
+  }
+}
