d5170a81f3ca49e0c61679ba139b446d5855c0c0
Le Vivilet <levivlet@pm.me>
2025-02-23 08:55:36 +0100

    feature: remove unused code

--- src/parts/SendPortToWebView/SendPortToWebView.ts
+++
@@ -1,6 +0,0 @@
-import * as Rpc from '../ParentRpc/ParentRpc.ts'
-
-// eslint-disable-next-line  @typescript-eslint/prefer-readonly-parameter-types
-export const sendPortToWebView = async (webviewId: string, port: MessagePort): Promise<void> => {
-  await Rpc.invokeAndTransfer('Transferrable.transferToRendererProcess', webviewId, port)
-}
