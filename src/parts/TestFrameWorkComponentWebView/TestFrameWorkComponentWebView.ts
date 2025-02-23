b89aabeffb1bf223a7242cce877c5418441d04c7
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-23 09:09:26 +0100

    feature: use messageportRpc module instead of custom ipc (#150)

--- src/parts/TestFrameWorkComponentWebView/TestFrameWorkComponentWebView.ts
+++ src/parts/TestFrameWorkComponentWebView/TestFrameWorkComponentWebView.ts
@@ -1,16 +1,10 @@
 import * as CreatePortIpc from '../CreatePortIpc/CreatePortIpc.ts'
-import * as JsonRpc from '../JsonRpc/JsonRpc.ts'
 import * as Locator from '../Locator/Locator.ts'
 import * as WebViewState from '../WebViewState/WebViewState.ts'
 
 export const fromId = async (webViewId: string): Promise<any> => {
-  const ipc = await CreatePortIpc.createPortIpc(webViewId)
-  const webViewRpc = {
-    invoke(method: string, ...params: readonly any[]): Promise<any> {
-      return JsonRpc.invoke(ipc, method, ...params)
-    },
-  }
-  WebViewState.set(webViewId, webViewRpc)
+  const rpc = await CreatePortIpc.createPortIpc(webViewId)
+  WebViewState.set(webViewId, rpc)
   return {
     locator(selector: string, options: any): any {
       const baseLocator = Locator.create(selector, options)
