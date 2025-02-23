db019210d9c4ccd2f52448a97d97c5b977083897
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-23 09:00:31 +0100

    feature: add types to expect module (#149)

--- src/parts/Expect/Expect.ts
+++ src/parts/Expect/Expect.ts
@@ -1,7 +1,4 @@
-// @ts-nocheck
-
 import * as GetLocatorRpc from '../GetLocatorRpc/GetLocatorRpc.ts'
-import * as Rpc from '../ParentRpc/ParentRpc.ts'
 
 const Assert = {
   string(value: any, message: string): void {
@@ -19,12 +16,11 @@ const Assert = {
 export const expect = (locator: any): any => {
   const { invoke } = GetLocatorRpc.getLocatorRpc(locator)
   return {
-    async checkSingleElementCondition(fnName, options): Promise<void> {
-      Assert.string(fnName)
+    async checkSingleElementCondition(fnName: string, options: any): Promise<void> {
       // TODO add rpcId property to locator instead
       return invoke('TestFrameWork.checkSingleElementCondition', locator, fnName, options)
     },
-    async checkMultiElementCondition(fnName, options): Promise<void> {
+    async checkMultiElementCondition(fnName: string, options: any): Promise<void> {
       return invoke('TestFrameWork.checkMultiElementCondition', locator, fnName, options)
     },
     async toBeVisible(): Promise<void> {
@@ -33,28 +29,28 @@ export const expect = (locator: any): any => {
       }
       return this.checkSingleElementCondition('toBeVisible', {})
     },
-    async toHaveText(text): Promise<void> {
+    async toHaveText(text: string): Promise<void> {
       Assert.string(text, 'text must be of type string')
       return this.checkSingleElementCondition('toHaveText', { text })
     },
-    async toContainText(text): Promise<void> {
+    async toContainText(text: string): Promise<void> {
       Assert.string(text, 'text must be of type string')
       return this.checkSingleElementCondition('toContainText', { text })
     },
-    async toHaveValue(value): Promise<void> {
+    async toHaveValue(value: string): Promise<void> {
       Assert.string(value, 'value must be of type string')
       return this.checkSingleElementCondition('toHaveValue', { value })
     },
     async toBeFocused(): Promise<void> {
       return this.checkSingleElementCondition('toBeFocused')
     },
-    async toHaveCSS(key, value): Promise<void> {
+    async toHaveCSS(key: string, value: string): Promise<void> {
       return this.checkSingleElementCondition('toHaveCss', {
         key,
         value,
       })
     },
-    async toHaveAttribute(key, value): Promise<void> {
+    async toHaveAttribute(key: string, value: string): Promise<void> {
       Assert.string(key, 'key must be of type string')
       // Assert.string(value, 'value must be of type string')
       return this.checkSingleElementCondition('toHaveAttribute', {
@@ -62,20 +58,20 @@ export const expect = (locator: any): any => {
         value,
       })
     },
-    async toHaveClass(className): Promise<void> {
+    async toHaveClass(className: string): Promise<void> {
       Assert.string(className, 'className must be of type string')
       return this.checkSingleElementCondition('toHaveClass', {
         className,
       })
     },
-    async toHaveId(id): Promise<void> {
+    async toHaveId(id: string): Promise<void> {
       Assert.string(id, 'id must be of type string')
       return this.checkSingleElementCondition('toHaveId', {
         id,
       })
     },
-    async toHaveCount(count): Promise<void> {
-      Assert.number(count, 'count must be of type string')
+    async toHaveCount(count: number): Promise<void> {
+      Assert.number(count, 'count must be of type number')
       return this.checkMultiElementCondition('toHaveCount', { count })
     },
     async toBeHidden(): Promise<void> {
