c5d5cb8ea2207300be8432559140c6df73f63d8a
Le Vivilet <levivlet@pm.me>
2025-02-23 09:08:16 +0100

    feature: use messageportRpc module instead of custom ipc

--- src/parts/CreatePortIpc/CreatePortIpc.ts
+++ src/parts/CreatePortIpc/CreatePortIpc.ts
@@ -1,21 +1,7 @@
+import { MessagePortRpcParent } from '@lvce-editor/rpc'
 import * as GetPortTuple from '../GetPortTuple/GetPortTuple.ts'
-import * as JsonRpc from '../JsonRpc/JsonRpc.ts'
 import * as ParentRpc from '../ParentRpc/ParentRpc.ts'
 import * as WaitForReadyEvent from '../WaitForReadyEvent/WaitForReadyEvent.ts'
-
-const preparePrettyError = (error: any): any => {
-  return error
-}
-
-const logError = (): void => {
-  // ignore
-}
-
-const execute = (): void => {}
-
-const requiresSocket = (): boolean => {
-  return false
-}
 
 export const createPortIpc = async (webViewId: string): Promise<any> => {
   const { port1, port2 } = GetPortTuple.getPortTuple()
@@ -32,15 +18,10 @@ export const createPortIpc = async (webViewId: string): Promise<any> => {
   if (firstEvent.data !== 'ready') {
     throw new Error('unexpected first message')
   }
-  const handleOtherMessage = async (event: any): Promise<void> => {
-    // @ts-ignore
-    await JsonRpc.handleJsonRpcMessage(ipc, event.data, JsonRpc.resolve, preparePrettyError, execute, logError, requiresSocket)
-  }
-  port1.onmessage = handleOtherMessage
-  const ipc = {
-    send(message: any): void {
-      port1.postMessage(message)
-    },
-  }
-  return ipc
+  const rpc = await MessagePortRpcParent.create({
+    messagePort: port1,
+    commandMap: {},
+    isMessagePortOpen: true,
+  })
+  return rpc
 }
--- src/parts/JsonRpc/JsonRpc.ts
+++
@@ -1,1 +0,0 @@
-export { handleJsonRpcMessage, invoke, resolve, send, invokeAndTransfer } from '@lvce-editor/json-rpc'
