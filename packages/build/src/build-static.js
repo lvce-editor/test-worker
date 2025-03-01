63324acb3b5801a36803fc7cc04623a05f82f151
Le Vivilet <levivlet@pm.me>
2025-03-01 16:51:42 +0100

    feature: improve build script

--- packages/build/src/build-static.js
+++ packages/build/src/build-static.js
@@ -9,24 +9,12 @@ const sharedProcessUrl = pathToFileURL(sharedProcessPath).toString()
 
 const sharedProcess = await import(sharedProcessUrl)
 
-process.env.PATH_PREFIX = '/title-bar-worker'
+process.env.PATH_PREFIX = '/test-worker'
 const { commitHash } = await sharedProcess.exportStatic({
   root,
   extensionPath: '',
 })
 
-await cp(
-  join(root, '.tmp', 'dist', 'dist', 'titleBarWorkerMain.js'),
-  join(root, 'dist', commitHash, 'packages', 'title-bar-worker-worker', 'dist', 'titleBarWorkerMain.js'),
-)
-
-const nodeModulesPath = join(root, 'packages', 'server', 'node_modules')
-
-const serverStaticPath = join(nodeModulesPath, '@lvce-editor', 'static-server', 'static')
-
-await cp(
-  join(serverStaticPath, commitHash, 'packages', 'renderer-worker', 'dist', 'rendererWorkerMain.js.original'),
-  join(root, 'dist', commitHash, 'packages', 'renderer-worker', 'dist', 'rendererWorkerMain.js'),
-)
+await cp(join(root, 'dist', 'dist', 'testWorkerMain.js'), join(root, 'dist', commitHash, 'packages', 'test-worker', 'dist', 'testWorkerMain.js'))
 
 await cp(join(root, 'dist'), join(root, '.tmp', 'static'), { recursive: true })
