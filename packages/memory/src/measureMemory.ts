1452916feaa7a163a85860ca5ffa43319ffe464c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-02 18:01:23 +0100

    feature: measure typescript memory (#169)

--- packages/memory/src/measureMemory.ts
+++ packages/memory/src/measureMemory.ts
@@ -1,11 +1,13 @@
 import { measureMemory } from '@lvce-editor/measure-memory'
-import { playwrightPath, threshold, workerPath } from './config.ts'
+import { instantiations, instantiationsPath, playwrightPath, threshold, workerPath } from './config.ts'
 
 const main = async () => {
   await measureMemory({
     playwrightPath,
     workerPath,
     threshold,
+    instantiations,
+    instantiationsPath,
   })
 }
 
