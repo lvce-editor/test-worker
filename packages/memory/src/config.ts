5eda09b9496236dda8d748eaf4ecd30f45620d90
Le Vivilet <levivlet@pm.me>
2025-03-02 17:57:03 +0100

    feature: measure typescript memory

--- packages/memory/src/config.ts
+++ packages/memory/src/config.ts
@@ -3,6 +3,10 @@ import { root } from './root.ts'
 
 export const threshold = 440_000
 
+export const instantiations = 500
+
+export const instantiationsPath = join(root, 'packages', 'test-worker')
+
 export const workerPath = join(root, 'dist/dist/testWorkerMain.js')
 
 export const playwrightPath = new URL('../../e2e/node_modules/playwright/index.mjs', import.meta.url).toString()
