75dc823f793aaf3989bd7c20378d23edddda3028
Le Vivilet <levivlet@pm.me>
2025-03-03 22:05:19 +0100

    feature: call getWebViewInfo2 to query webview info

--- packages/test-worker/src/parts/GetWebViewInfo/GetWebViewInfo.ts
+++ packages/test-worker/src/parts/GetWebViewInfo/GetWebViewInfo.ts
@@ -1,6 +1,6 @@
 import * as ParentRpc from '../ParentRpc/ParentRpc.ts'
 
 export const getWebViewInfo = async (webViewId: string): Promise<any> => {
-  const info = await ParentRpc.invoke('WebView.getWebViewInfo', webViewId)
+  const info = await ParentRpc.invoke('WebView.getWebViewInfo2', webViewId)
   return info
 }
