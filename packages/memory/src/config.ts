164ff36f66e2b5835a01ff578e67041bd31643ae
Le Vivilet <levivlet@pm.me>
2025-03-09 11:57:10 +0100

    memory

--- packages/memory/src/config.ts
+++ packages/memory/src/config.ts
@@ -3,7 +3,7 @@ import { root } from './root.ts'
 
 export const threshold = 450_000
 
-export const instantiations = 600
+export const instantiations = 700
 
 export const instantiationsPath = join(root, 'packages', 'test-worker')
 
