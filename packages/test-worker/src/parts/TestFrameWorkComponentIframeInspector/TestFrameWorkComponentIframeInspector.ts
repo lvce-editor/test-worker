992d37c4b52ce5f65ea0afd4667d5815c5c85bbb
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-15 10:23:56 +0100

    feature: add iframe inspector page object model (#203)

---
+++ packages/test-worker/src/parts/TestFrameWorkComponentIframeInspector/TestFrameWorkComponentIframeInspector.ts
@@ -0,0 +1,21 @@
+import * as Rpc from '../ParentRpc/ParentRpc.ts'
+
+export const selectIndex = async (index: number): Promise<void> => {
+  return Rpc.invoke('IframeInspector.selectIndex', index)
+}
+
+export const focusNext = async (): Promise<void> => {
+  return Rpc.invoke('IframeInspector.focusNext')
+}
+
+export const focusPrevious = async (): Promise<void> => {
+  return Rpc.invoke('IframeInspector.focusPrevious')
+}
+
+export const focusFirst = async (): Promise<void> => {
+  return Rpc.invoke('IframeInspector.focusFirst')
+}
+
+export const focusLast = async (): Promise<void> => {
+  return Rpc.invoke('IframeInspector.focusLast')
+}
