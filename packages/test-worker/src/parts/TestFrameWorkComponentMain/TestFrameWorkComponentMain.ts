608e5686d700944220fba6416e8dd26ce1a96fee
Le Vivilet <levivlet@pm.me>
2025-03-15 10:00:16 +0100

    feature: add closeAllEditors command

--- packages/test-worker/src/parts/TestFrameWorkComponentMain/TestFrameWorkComponentMain.ts
+++ packages/test-worker/src/parts/TestFrameWorkComponentMain/TestFrameWorkComponentMain.ts
@@ -11,3 +11,39 @@ export const splitRight = async (): Promise<void> => {
 export const openKeyBindings = async (): Promise<void> => {
   await Rpc.invoke('Main.openKeyBindings')
 }
+
+export const closeAllEditors = async (): Promise<void> => {
+  await Rpc.invoke('Main.closeAllEditors')
+}
+
+export const closeTabsLeft = async (): Promise<void> => {
+  await Rpc.invoke('Main.closeTabsLeft')
+}
+
+export const closeTabsRight = async (): Promise<void> => {
+  await Rpc.invoke('Main.closeTabsRight')
+}
+
+export const closeOthers = async (): Promise<void> => {
+  await Rpc.invoke('Main.closeOthers')
+}
+
+export const closeActiveEditor = async (): Promise<void> => {
+  await Rpc.invoke('Main.closeActiveEditor')
+}
+
+export const focusFirst = async (): Promise<void> => {
+  await Rpc.invoke('Main.focusFirst')
+}
+
+export const focusNext = async (): Promise<void> => {
+  await Rpc.invoke('Main.focusNext')
+}
+
+export const focusPrevious = async (): Promise<void> => {
+  await Rpc.invoke('Main.focusPrevious')
+}
+
+export const focusLast = async (): Promise<void> => {
+  await Rpc.invoke('Main.focusLast')
+}
