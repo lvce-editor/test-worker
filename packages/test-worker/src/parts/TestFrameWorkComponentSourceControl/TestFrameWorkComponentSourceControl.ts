09ce259191dbc321e8ed55cf3e3e78027cc2d20b
Le Vivilet <levivlet@pm.me>
2025-05-04 19:39:59 +0200

    feature: add source control button click command

--- packages/test-worker/src/parts/TestFrameWorkComponentSourceControl/TestFrameWorkComponentSourceControl.ts
+++ packages/test-worker/src/parts/TestFrameWorkComponentSourceControl/TestFrameWorkComponentSourceControl.ts
@@ -7,3 +7,7 @@ export const acceptInput = async (): Promise<void> => {
 export const handleInput = async (text: string): Promise<void> => {
   await Rpc.invoke('Source Control.handleInput', text)
 }
+
+export const handleClickSourceControlButtons = async (index: number, name: string): Promise<void> => {
+  await Rpc.invoke('Source Control.handleClickSourceControlButtons', index, name)
+}
