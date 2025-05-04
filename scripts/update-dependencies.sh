208f38edb8d72f47c7fec051b11719cb21379816
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-04 19:44:25 +0200

    update dependencies (#271)

--- scripts/update-dependencies.sh
+++ scripts/update-dependencies.sh
@@ -31,6 +31,7 @@ cd packages/build                                   && updateDependencies && cd 
 cd packages/e2e                                     && updateDependencies && cd ../.. &&
 cd packages/memory                                  && updateDependencies && cd ../.. &&
 cd packages/server                                  && updateDependencies && cd ../.. &&
+cd packages/test-worker                             && updateDependencies && cd ../.. &&
 
 echo "Great Success!"
 
