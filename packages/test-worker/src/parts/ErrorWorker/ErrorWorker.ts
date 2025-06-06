5620a02547fd5fbc9609b503350919b8fe539958
Le Vivilet <levivlet@pm.me>
2025-06-06 18:15:14 +0200

    feature: ask error worker to create codeframe

---
+++ packages/test-worker/src/parts/ErrorWorker/ErrorWorker.ts
@@ -0,0 +1,3 @@
+import { ErrorWorker } from '@lvce-editor/rpc-registry'
+
+export const { set, invoke, invokeAndTransfer } = ErrorWorker
--- packages/test-worker/src/parts/PrintTestError/PrintTestError.ts
+++ packages/test-worker/src/parts/PrintTestError/PrintTestError.ts
@@ -1,6 +1,9 @@
 import { printError } from '../PrintError/PrintError.ts'
+import * as ErrorWorker from '../ErrorWorker/ErrorWorker.ts'
 
 export const printTestError = async (error: any): Promise<void> => {
+  const prettyError = await ErrorWorker.invoke('Errors.prepare', error)
+  console.log({ prettyError })
   // TODO ask error worker to add codeframe
   printError(error)
 }
