70e41d8e436d9c47901e777592af571cb007b395
Le Vivilet <levivlet@pm.me>
2025-07-09 23:54:31 +0200

    feature: add extensionDetail openFeature command

--- packages/test-worker/src/parts/TestFrameWorkComponentExtensionDetail/TestFrameWorkComponentExtensionDetail.ts
+++ packages/test-worker/src/parts/TestFrameWorkComponentExtensionDetail/TestFrameWorkComponentExtensionDetail.ts
@@ -12,3 +12,19 @@ export const open = (extensionId: string): Promise<void> => {
   const uri = `extension-detail://${extensionId}`
   return Rpc.invoke('Main.openUri', uri)
 }
+
+export const openFeature = (featureName: string): Promise<void> => {
+  return Rpc.invoke('ExtensionDetail.handleFeaturesClick', featureName)
+}
+
+export const openThemes = async (): Promise<void> => {
+  await openFeature('Themes')
+}
+
+export const openCommands = async (): Promise<void> => {
+  await openFeature('Commands')
+}
+
+export const openWebViews = async (): Promise<void> => {
+  await openFeature('WebView')
+}
