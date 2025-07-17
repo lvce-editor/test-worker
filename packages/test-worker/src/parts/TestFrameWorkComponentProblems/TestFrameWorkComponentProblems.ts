26c022b1276cc79245aaee0ca9bfa39b90128642
Le Vivilet <levivlet@pm.me>
2025-07-17 10:30:23 +0200

    feature: add more problem commands

--- packages/test-worker/src/parts/TestFrameWorkComponentProblems/TestFrameWorkComponentProblems.ts
+++ packages/test-worker/src/parts/TestFrameWorkComponentProblems/TestFrameWorkComponentProblems.ts
@@ -4,3 +4,48 @@ export const show = async (): Promise<void> => {
   // @ts-ignore
   await Rpc.invoke('Panel.selectIndex', 0)
 }
+
+export const handleFilterInput = async (text: string): Promise<void> => {
+  // @ts-ignore
+  await Rpc.invoke('Problems.handleFilterInput', text)
+}
+
+export const copyMessage = async (): Promise<void> => {
+  // @ts-ignore
+  await Rpc.invoke('Problems.copyMessage')
+}
+
+export const focusIndex = async (index: number): Promise<void> => {
+  // @ts-ignore
+  await Rpc.invoke('Problems.focusIndex', index)
+}
+
+export const handleArrowLeft = async (): Promise<void> => {
+  // @ts-ignore
+  await Rpc.invoke('Problems.handleArrowLeft')
+}
+
+export const handleArrowRight = async (): Promise<void> => {
+  // @ts-ignore
+  await Rpc.invoke('Problems.handleArrowRight')
+}
+
+export const handleClickAt = async (x: number, y: number): Promise<void> => {
+  // @ts-ignore
+  await Rpc.invoke('Problems.handleClickAt', x, y)
+}
+
+export const handleIconThemeChange = async (): Promise<void> => {
+  // @ts-ignore
+  await Rpc.invoke('Problems.handleIconThemeChange')
+}
+
+export const viewAsList = async (): Promise<void> => {
+  // @ts-ignore
+  await Rpc.invoke('Problems.viewAsList')
+}
+
+export const viewAsTable = async (): Promise<void> => {
+  // @ts-ignore
+  await Rpc.invoke('Problems.viewAsTable')
+}
