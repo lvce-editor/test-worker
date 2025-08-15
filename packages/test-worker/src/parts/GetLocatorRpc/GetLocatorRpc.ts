1dc668f95d78c14772ee87e2129c378847fce33d
Le Vivilet <levivlet@pm.me>
2025-08-15 17:17:00 +0200

    feature: rename module

--- packages/test-worker/src/parts/GetLocatorRpc/GetLocatorRpc.ts
+++ packages/test-worker/src/parts/GetLocatorRpc/GetLocatorRpc.ts
@@ -1,9 +1,9 @@
-import * as Rpc from '../RendererWorker/RendererWorker.ts'
+import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 import * as WebViewState from '../WebViewState/WebViewState.ts'
 
 export const getLocatorRpc = (locator: any): any => {
   if (locator.webViewId) {
     return WebViewState.get(locator.webViewId)
   }
-  return Rpc
+  return RendererWorker
 }
