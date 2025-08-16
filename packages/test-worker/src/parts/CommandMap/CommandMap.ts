a9459b97140c5fd41854e337f46142f54d8904f4
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-16 22:10:33 +0200

    feature: add executeMock command (#396)

--- packages/test-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/test-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,5 +1,7 @@
 import * as Test from '../Test/Test.ts'
+import * as TestFrameWorkComponentDialog from '../TestFrameWorkComponentDialog/TestFrameWorkComponentDialog.ts'
 
 export const commandMap = {
   'Test.execute': Test.execute,
+  'Test.executeMock': TestFrameWorkComponentDialog.executeMock,
 }
