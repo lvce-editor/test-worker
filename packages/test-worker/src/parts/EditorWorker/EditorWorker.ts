1a7483226e994c91a2f2d411ff1c1dfdf337a48f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-18 14:33:14 +0200

    feature: add editor worker module (#406)
    * feature: add editor worker module
    
    * lint
    
    * lint

---
+++ packages/test-worker/src/parts/EditorWorker/EditorWorker.ts
@@ -0,0 +1,16 @@
+import type { Rpc } from '@lvce-editor/rpc'
+import { launchEditorWorkerRpc } from '../LaunchEditorWorkerRpc/LaunchEditorWorkerRpc.ts'
+
+let rpcPromise: Promise<Rpc> | undefined
+
+const getRpc = (): Promise<Rpc> => {
+  if (!rpcPromise) {
+    rpcPromise = launchEditorWorkerRpc()
+  }
+  return rpcPromise
+}
+
+export const invoke = async (method: string, ...params: readonly any[]): Promise<any> => {
+  const rpc = await getRpc()
+  return rpc.invoke(method, ...params)
+}
---
+++ packages/test-worker/src/parts/LaunchEditorWorkerRpc/LaunchEditorWorkerRpc.ts
@@ -0,0 +1,16 @@
+import type { Rpc} from '@lvce-editor/rpc';
+import { TransferMessagePortRpcParent } from '@lvce-editor/rpc'
+import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
+
+const send = async (port: MessagePort): Promise<void> => {
+  const rpcId = 9001
+  await RendererWorker.sendMessagePortToEditorWorker(port, rpcId)
+}
+
+export const launchEditorWorkerRpc = async (): Promise<Rpc> => {
+  const rpc = await TransferMessagePortRpcParent.create({
+    commandMap: {},
+    send,
+  })
+  return rpc
+}
