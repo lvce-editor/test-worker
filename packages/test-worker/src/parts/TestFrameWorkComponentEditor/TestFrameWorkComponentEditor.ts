f0390c97ce75956a8b52b88668a3bf2b471f8c57
Le Vivilet <levivlet@pm.me>
2025-08-18 14:22:59 +0200

    feature: rename module

--- packages/test-worker/src/parts/TestFrameWorkComponentEditor/TestFrameWorkComponentEditor.ts
+++ packages/test-worker/src/parts/TestFrameWorkComponentEditor/TestFrameWorkComponentEditor.ts
@@ -1,234 +1,234 @@
-import * as Rpc from '../RendererWorker/RendererWorker.ts'
+import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 
 export const setCursor = async (rowIndex: number, columnIndex: number): Promise<void> => {
-  await Rpc.invoke('Editor.cursorSet', rowIndex, columnIndex)
+  await RendererWorker.invoke('Editor.cursorSet', rowIndex, columnIndex)
 }
 
 export const openCompletion = async (): Promise<void> => {
-  await Rpc.invoke('Editor.openCompletion')
+  await RendererWorker.invoke('Editor.openCompletion')
 }
 
 export const closeCompletion = async (): Promise<void> => {
   // @ts-ignore
-  await Rpc.invoke('EditorCompletion.close')
+  await RendererWorker.invoke('EditorCompletion.close')
 }
 
 export const openEditorContextMenu = async (): Promise<void> => {
-  await Rpc.invoke('Editor.handleContextMenu', 0, 0)
+  await RendererWorker.invoke('Editor.handleContextMenu', 0, 0)
 }
 
 export const invokeTabCompletion = async (): Promise<void> => {
-  await Rpc.invoke('Editor.tabCompletion')
+  await RendererWorker.invoke('Editor.tabCompletion')
 }
 
 export const executeTabCompletion = async (): Promise<void> => {
-  await Rpc.invoke('Editor.tabCompletion')
+  await RendererWorker.invoke('Editor.tabCompletion')
 }
 
 export const invokeBraceCompletion = async (text: string): Promise<void> => {
-  await Rpc.invoke('Editor.braceCompletion', text)
+  await RendererWorker.invoke('Editor.braceCompletion', text)
 }
 
 export const cursorCharacterRight = async (): Promise<void> => {
-  await Rpc.invoke('Editor.cursorCharacterRight')
+  await RendererWorker.invoke('Editor.cursorCharacterRight')
 }
 
 export const cursorCharacterLeft = async (): Promise<void> => {
-  await Rpc.invoke('Editor.cursorCharacterLeft')
+  await RendererWorker.invoke('Editor.cursorCharacterLeft')
 }
 
 export const copyLineDown = async (): Promise<void> => {
-  await Rpc.invoke('Editor.copyLineDown')
+  await RendererWorker.invoke('Editor.copyLineDown')
 }
 
 export const cursorDown = async (): Promise<void> => {
-  await Rpc.invoke('Editor.cursorDown')
+  await RendererWorker.invoke('Editor.cursorDown')
 }
 
 export const cursorUp = async (): Promise<void> => {
-  await Rpc.invoke('Editor.cursorUp')
+  await RendererWorker.invoke('Editor.cursorUp')
 }
 
 export const cursorWordLeft = async (): Promise<void> => {
-  await Rpc.invoke('Editor.cursorWordLeft')
+  await RendererWorker.invoke('Editor.cursorWordLeft')
 }
 
 export const cursorWordRight = async (): Promise<void> => {
-  await Rpc.invoke('Editor.cursorWordRight')
+  await RendererWorker.invoke('Editor.cursorWordRight')
 }
 
 export const goToDefinition = async (): Promise<void> => {
-  await Rpc.invoke('Editor.goToDefinition')
+  await RendererWorker.invoke('Editor.goToDefinition')
 }
 
 export const openHover = async (): Promise<void> => {
-  await Rpc.invoke('Editor.showHover2')
+  await RendererWorker.invoke('Editor.showHover2')
 }
 
 export const goToTypeDefinition = async (): Promise<void> => {
-  await Rpc.invoke('Editor.goToTypeDefinition')
+  await RendererWorker.invoke('Editor.goToTypeDefinition')
 }
 
 export const type = async (text: string): Promise<void> => {
-  await Rpc.invoke('Editor.type', text)
+  await RendererWorker.invoke('Editor.type', text)
 }
 
 export const findAllReferences = async (): Promise<void> => {
   // @ts-ignore
-  await Rpc.invoke('SideBar.show', 'References', /* focus */ true)
+  await RendererWorker.invoke('SideBar.show', 'References', /* focus */ true)
 }
 
 export const findAllImplementations = async (): Promise<void> => {
   // @ts-ignore
-  await Rpc.invoke('SideBar.show', 'Implementations', /* focus */ true)
+  await RendererWorker.invoke('SideBar.show', 'Implementations', /* focus */ true)
 }
 
 export const setSelections = async (selections: any): Promise<void> => {
-  await Rpc.invoke('Editor.setSelections', selections)
+  await RendererWorker.invoke('Editor.setSelections', selections)
 }
 
 export const openFindWidget = async (): Promise<void> => {
-  await Rpc.invoke('Editor.openFind')
+  await RendererWorker.invoke('Editor.openFind')
 }
 
 export const setDeltaY = async (deltaY: number): Promise<void> => {
-  await Rpc.invoke('Editor.setDeltaY', deltaY)
+  await RendererWorker.invoke('Editor.setDeltaY', deltaY)
 }
 
 export const format = async (): Promise<void> => {
-  await Rpc.invoke('Editor.format')
+  await RendererWorker.invoke('Editor.format')
 }
 
 export const insertLineBreak = async (): Promise<void> => {
-  await Rpc.invoke('Editor.insertLineBreak')
+  await RendererWorker.invoke('Editor.insertLineBreak')
 }
 
 export const openSourceActions = async (): Promise<void> => {
-  await Rpc.invoke('Editor.showSourceActions2')
+  await RendererWorker.invoke('Editor.showSourceActions2')
 }
 
 export const sourceActionsSelectCurrent = async (): Promise<void> => {
-  await Rpc.invoke('EditorSourceActions.selectCurrent')
+  await RendererWorker.invoke('EditorSourceActions.selectCurrent')
 }
 
 export const openCompletionDetails = async (): Promise<void> => {
-  await Rpc.invoke('EditorCompletion.openDetails')
+  await RendererWorker.invoke('EditorCompletion.openDetails')
 }
 
 export const closeCompletionDetails = async (): Promise<void> => {
   // @ts-ignore
-  await Rpc.invoke('EditorCompletion.closeDetails')
+  await RendererWorker.invoke('EditorCompletion.closeDetails')
 }
 
 export const toggleCompletionDetails = async (): Promise<void> => {
   // @ts-ignore
-  await Rpc.invoke('EditorCompletion.toggleDetails')
+  await RendererWorker.invoke('EditorCompletion.toggleDetails')
 }
 
 export const organizeImports = async (): Promise<void> => {
-  await Rpc.invoke('Editor.organizeImports')
+  await RendererWorker.invoke('Editor.organizeImports')
 }
 
 export const addAllMissingImports = async (): Promise<void> => {
-  await Rpc.invoke('Editor.addAllMissingImports')
+  await RendererWorker.invoke('Editor.addAllMissingImports')
 }
 
 export const sortImports = async (): Promise<void> => {
-  await Rpc.invoke('Editor.sortImports')
+  await RendererWorker.invoke('Editor.sortImports')
 }
 
 export const toggleLineComment = async (): Promise<void> => {
-  await Rpc.invoke('Editor.toggleLineComment')
+  await RendererWorker.invoke('Editor.toggleLineComment')
 }
 
 export const toggleBlockComment = async (): Promise<void> => {
-  await Rpc.invoke('Editor.toggleBlockComment')
+  await RendererWorker.invoke('Editor.toggleBlockComment')
 }
 
 export const selectAll = async (): Promise<void> => {
-  await Rpc.invoke('Editor.toggleBlockComment')
+  await RendererWorker.invoke('Editor.toggleBlockComment')
 }
 
 export const openColorPicker = async (): Promise<void> => {
-  await Rpc.invoke('Editor.openColorPicker')
+  await RendererWorker.invoke('Editor.openColorPicker')
 }
 
 export const openFind = async (): Promise<void> => {
   // @ts-ignore
-  await Rpc.invoke('Editor.openFind2')
+  await RendererWorker.invoke('Editor.openFind2')
 }
 
 export const deleteAllLeft = async (): Promise<void> => {
-  await Rpc.invoke('Editor.deleteAllLeft')
+  await RendererWorker.invoke('Editor.deleteAllLeft')
 }
 
 export const deleteAllRight = async (): Promise<void> => {
-  await Rpc.invoke('Editor.deleteAllRight')
+  await RendererWorker.invoke('Editor.deleteAllRight')
 }
 
 export const cursorWordPartLeft = async (): Promise<void> => {
-  await Rpc.invoke('Editor.cursorWordPartLeft')
+  await RendererWorker.invoke('Editor.cursorWordPartLeft')
 }
 
 export const cursorWordPartRight = async (): Promise<void> => {
-  await Rpc.invoke('Editor.cursorWordPartRight')
+  await RendererWorker.invoke('Editor.cursorWordPartRight')
 }
 
 export const cursorEnd = async (): Promise<void> => {
-  await Rpc.invoke('Editor.cursorEnd')
+  await RendererWorker.invoke('Editor.cursorEnd')
 }
 
 export const cursorHome = async (): Promise<void> => {
-  await Rpc.invoke('Editor.cursorHome')
+  await RendererWorker.invoke('Editor.cursorHome')
 }
 
 export const copyLineUp = async (): Promise<void> => {
-  await Rpc.invoke('Editor.copyLineUp')
+  await RendererWorker.invoke('Editor.copyLineUp')
 }
 
 export const copy = async (): Promise<void> => {
-  await Rpc.invoke('Editor.copy')
+  await RendererWorker.invoke('Editor.copy')
 }
 
 export const closeColorPicker = async (): Promise<void> => {
-  await Rpc.invoke('Editor.closeColorPicker')
+  await RendererWorker.invoke('Editor.closeColorPicker')
 }
 
 export const openContextMenu = async (): Promise<void> => {
   const button = 0
   const x = 0
   const y = 0
-  await Rpc.invoke('Editor.contextMenu', button, x, y)
+  await RendererWorker.invoke('Editor.contextMenu', button, x, y)
 }
 
 export const getText = async (): Promise<string> => {
-  return Rpc.invoke('Editor.getText')
+  return RendererWorker.invoke('Editor.getText')
 }
 
 export const rename = async (): Promise<void> => {
-  await Rpc.invoke('Editor.rename')
+  await RendererWorker.invoke('Editor.rename')
 }
 
 export const showHover = async (): Promise<void> => {
-  await Rpc.invoke('Editor.showHover2')
+  await RendererWorker.invoke('Editor.showHover2')
 }
 
 export const openRename = async (): Promise<void> => {
-  await Rpc.invoke('Editor.openRename')
+  await RendererWorker.invoke('Editor.openRename')
 }
 
 export const growSelection = async (): Promise<void> => {
   // @ts-ignore
-  await Rpc.invoke('Editor.selectionGrow')
+  await RendererWorker.invoke('Editor.selectionGrow')
 }
 
 export const getSelections = async (): Promise<Uint32Array> => {
   // @ts-ignore
-  return Rpc.invoke('Editor.getSelections')
+  return RendererWorker.invoke('Editor.getSelections')
 }
 
 export const shouldHaveText = async (expectedText: string): Promise<void> => {
-  const text = await Rpc.invoke('Editor.getText')
+  const text = await RendererWorker.invoke('Editor.getText')
   if (text !== expectedText) {
     throw new Error(`Expected editor to have text ${expectedText} but was ${text}`)
   }
