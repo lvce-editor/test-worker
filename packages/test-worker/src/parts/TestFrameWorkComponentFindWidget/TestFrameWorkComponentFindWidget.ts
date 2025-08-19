50055a9474a8dfdb437487f673be1ccb6030203e
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-19 17:08:17 +0200

    feature: rename variable (#410)

--- packages/test-worker/src/parts/TestFrameWorkComponentFindWidget/TestFrameWorkComponentFindWidget.ts
+++ packages/test-worker/src/parts/TestFrameWorkComponentFindWidget/TestFrameWorkComponentFindWidget.ts
@@ -1,9 +1,9 @@
-import * as Rpc from '../RendererWorker/RendererWorker.ts'
+import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 
 export const focusNext = async (): Promise<void> => {
-  await Rpc.invoke('FindWidget.focusNext')
+  await RendererWorker.invoke('FindWidget.focusNext')
 }
 
 export const setValue = async (value: string): Promise<void> => {
-  await Rpc.invoke('FindWidget.handleInput', value)
+  await RendererWorker.invoke('FindWidget.handleInput', value)
 }
