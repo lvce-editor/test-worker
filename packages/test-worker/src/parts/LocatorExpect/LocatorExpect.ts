22a0a8686a63c5e41a2227a173cabfb470f00a46
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-30 23:47:47 +0200

    feature: add locatorExpect type (#428)

---
+++ packages/test-worker/src/parts/LocatorExpect/LocatorExpect.ts
@@ -0,0 +1,17 @@
+export interface LocatorExpect {
+  readonly checkSingleElementCondition: (fnName: string, options: any) => Promise<void>
+  readonly checkMultiElementCondition: (fnName: string, options: any) => Promise<void>
+  readonly toBeVisible: () => Promise<void>
+  readonly toHaveText: (text: string) => Promise<void>
+  readonly toContainText: (text: string) => Promise<void>
+  readonly toHaveValue: (value: string) => Promise<void>
+  readonly toBeFocused: () => Promise<void>
+  readonly toHaveCSS: (key: string, value: string) => Promise<void>
+  readonly toHaveAttribute: (key: string, value: string) => Promise<void>
+  readonly toHaveJSProperty: (key: string, value: any) => Promise<void>
+  readonly toHaveClass: (className: string) => Promise<void>
+  readonly toHaveId: (id: string) => Promise<void>
+  readonly toHaveCount: (count: number) => Promise<void>
+  readonly toBeHidden: () => Promise<void>
+  readonly not: () => any
+}
