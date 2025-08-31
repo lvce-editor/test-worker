9d088190f232d188bb06b765756282a04c788887
Le Vivilet <levivlet@pm.me>
2025-08-31 08:38:16 +0200

    test

--- packages/test-worker/test/GetPortTuple.test.ts
+++ packages/test-worker/test/GetPortTuple.test.ts
@@ -1,9 +1,4 @@
 import { expect, test } from '@jest/globals'
-import { MessageChannel as NodeMessageChannel, MessagePort as NodeMessagePort } from 'worker_threads'
-
-// Polyfill global MessageChannel/MessagePort for Node test environment
-;(globalThis as any).MessageChannel = (globalThis as any).MessageChannel || NodeMessageChannel
-;(globalThis as any).MessagePort = (globalThis as any).MessagePort || NodeMessagePort
 
 import * as GetPortTuple from '../src/parts/GetPortTuple/GetPortTuple.ts'
 
