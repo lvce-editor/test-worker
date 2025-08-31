3104e71ffba9fb69d1e2f940050b1593d6bbda28
Le Vivilet <levivlet@pm.me>
2025-08-31 15:18:49 +0200

    lint

--- packages/test-worker/test/TestFrameWorkComponentStatusBar.test.ts
+++ packages/test-worker/test/TestFrameWorkComponentStatusBar.test.ts
@@ -4,10 +4,10 @@ import { RendererWorker } from '@lvce-editor/rpc-registry'
 import * as StatusBar from '../src/parts/TestFrameWorkComponentStatusBar/TestFrameWorkComponentStatusBar.ts'
 
 test('update calls StatusBar.updateStatusBarItems', async () => {
-	const invoke: jest.Mock = jest.fn()
-	const mockRpc = MockRpc.create({ commandMap: {}, invoke })
-	RendererWorker.set(mockRpc)
-	await StatusBar.update()
-	expect(invoke).toHaveBeenCalledTimes(1)
-	expect(invoke).toHaveBeenCalledWith('StatusBar.updateStatusBarItems')
+  const invoke: jest.Mock = jest.fn()
+  const mockRpc = MockRpc.create({ commandMap: {}, invoke })
+  RendererWorker.set(mockRpc)
+  await StatusBar.update()
+  expect(invoke).toHaveBeenCalledTimes(1)
+  expect(invoke).toHaveBeenCalledWith('StatusBar.updateStatusBarItems')
 })
--- packages/test-worker/test/TestState.test.ts
+++ packages/test-worker/test/TestState.test.ts
@@ -2,25 +2,25 @@ import { expect, jest, test } from '@jest/globals'
 import * as TestState from '../src/parts/TestState/TestState.ts'
 
 const createMockRpc = (name: string): any => {
-	return { name, invoke: jest.fn() }
+  return { name, invoke: jest.fn() }
 }
 
 test('addTest and getTests empties queue', () => {
-	TestState.addTest('t1', () => {})
-	TestState.addTest('t2', () => {})
-	const tests1 = TestState.getTests()
-	expect(Array.isArray(tests1)).toBe(true)
-	expect(tests1.map((t: any) => t.name)).toEqual(['t1', 't2'])
-	const tests2 = TestState.getTests()
-	expect(tests2).toEqual([])
+  TestState.addTest('t1', () => {})
+  TestState.addTest('t2', () => {})
+  const tests1 = TestState.getTests()
+  expect(Array.isArray(tests1)).toBe(true)
+  expect(tests1.map((t: any) => t.name)).toEqual(['t1', 't2'])
+  const tests2 = TestState.getTests()
+  expect(tests2).toEqual([])
 })
 
 test('setMockRpc and getMockRpc by name', () => {
-	const rpcA = createMockRpc('A')
-	const rpcB = createMockRpc('B')
-	TestState.setMockRpc(rpcA)
-	TestState.setMockRpc(rpcB)
-	expect(TestState.getMockRpc('A')).toBe(rpcA)
-	expect(TestState.getMockRpc('B')).toBe(rpcB)
-	expect(TestState.getMockRpc('C')).toBeUndefined()
+  const rpcA = createMockRpc('A')
+  const rpcB = createMockRpc('B')
+  TestState.setMockRpc(rpcA)
+  TestState.setMockRpc(rpcB)
+  expect(TestState.getMockRpc('A')).toBe(rpcA)
+  expect(TestState.getMockRpc('B')).toBe(rpcB)
+  expect(TestState.getMockRpc('C')).toBeUndefined()
 })
