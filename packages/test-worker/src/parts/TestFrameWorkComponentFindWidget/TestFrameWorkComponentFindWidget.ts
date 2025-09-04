f4f8bfcc99b9f838adcdda466fd10ae25e060bff
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-09-04 15:14:05 +0200

    feature: add inputSource to find widget (#450)
    * feature: add inputSource to find widget
    
    * test
    
    ---------
    
    Co-authored-by: Le Vivilet <levivlet@pm.me>

--- packages/test-worker/src/parts/TestFrameWorkComponentFindWidget/TestFrameWorkComponentFindWidget.ts
+++ packages/test-worker/src/parts/TestFrameWorkComponentFindWidget/TestFrameWorkComponentFindWidget.ts
@@ -1,9 +1,16 @@
 import { RendererWorker } from '@lvce-editor/rpc-registry'
+import * as InputSource from '../InputSource/InputSource.ts'
 
 export const focusNext = async (): Promise<void> => {
   await RendererWorker.invoke('FindWidget.focusNext')
 }
 
+export const focusPrevious = async (): Promise<void> => {
+  // @ts-ignore
+  await RendererWorker.invoke('FindWidget.focusPrevious')
+}
+
 export const setValue = async (value: string): Promise<void> => {
-  await RendererWorker.invoke('FindWidget.handleInput', value)
+  // @ts-ignore
+  await RendererWorker.invoke('FindWidget.handleInput', value, InputSource.Script)
 }
