072ffcc1ed4599992ac1dc97cccc638df7a12fe6
Le Vivilet <levivilet@pm.me>
2025-09-07 15:01:59 +0200

    feature: add explorer collapseAll (#458)

--- packages/test-worker/src/parts/TestFrameWorkComponentExplorer/TestFrameWorkComponentExplorer.ts
+++ packages/test-worker/src/parts/TestFrameWorkComponentExplorer/TestFrameWorkComponentExplorer.ts
@@ -1,87 +1,92 @@
 /* eslint-disable @typescript-eslint/prefer-readonly-parameter-types */
-import { RendererWorker as Rpc } from '@lvce-editor/rpc-registry'
+import { RendererWorker } from '@lvce-editor/rpc-registry'
 
 export const openContextMenu = async (index: number): Promise<void> => {
   // @ts-ignore
-  await Rpc.invoke('Explorer.handleContextMenuKeyboard', index)
+  await RendererWorker.invoke('Explorer.handleContextMenuKeyboard', index)
 }
 
 export const handleDragLeave = async (): Promise<void> => {
-  await Rpc.invoke('Explorer.handleDragLeave')
+  await RendererWorker.invoke('Explorer.handleDragLeave')
 }
 
 export const handleBlur = async (): Promise<void> => {
-  await Rpc.invoke('Explorer.handleBlur')
+  await RendererWorker.invoke('Explorer.handleBlur')
 }
 
 export const handleInputBlur = async (): Promise<void> => {
-  await Rpc.invoke('Explorer.handleInputBlur')
+  await RendererWorker.invoke('Explorer.handleInputBlur')
 }
 
 export const focus = async (): Promise<void> => {
-  await Rpc.invoke('Explorer.focusIndex', -1)
+  await RendererWorker.invoke('Explorer.focusIndex', -1)
 }
 
 export const focusNext = async (): Promise<void> => {
   // @ts-ignore
-  await Rpc.invoke('Explorer.focusNext')
+  await RendererWorker.invoke('Explorer.focusNext')
 }
 
 export const selectUp = async (): Promise<void> => {
-  await Rpc.invoke('Explorer.selectUp')
+  await RendererWorker.invoke('Explorer.selectUp')
 }
 
 export const handleDragOverIndex = async (index: number): Promise<void> => {
   // @ts-ignore
-  await Rpc.invoke('Explorer.handleDragOverIndex', index)
+  await RendererWorker.invoke('Explorer.handleDragOverIndex', index)
 }
 
 export const selectDown = async (): Promise<void> => {
-  await Rpc.invoke('Explorer.selectDown')
+  await RendererWorker.invoke('Explorer.selectDown')
+}
+
+export const collapseAll = async (): Promise<void> => {
+  // @ts-ignore
+  await RendererWorker.invoke('Explorer.collapseAll')
 }
 
 export const refresh = async (): Promise<void> => {
-  await Rpc.invoke('Explorer.refresh')
+  await RendererWorker.invoke('Explorer.refresh')
 }
 
 export const focusIndex = async (index: number): Promise<void> => {
-  await Rpc.invoke('Explorer.focusIndex', index)
+  await RendererWorker.invoke('Explorer.focusIndex', index)
 }
 
 export const clickCurrent = async (): Promise<void> => {
-  await Rpc.invoke('Explorer.handleClickCurrent')
+  await RendererWorker.invoke('Explorer.handleClickCurrent')
 }
 
 export const handleArrowLeft = async (): Promise<void> => {
-  await Rpc.invoke('Explorer.handleArrowLeft')
+  await RendererWorker.invoke('Explorer.handleArrowLeft')
 }
 
 export const focusLast = async (): Promise<void> => {
-  await Rpc.invoke('Explorer.focusLast')
+  await RendererWorker.invoke('Explorer.focusLast')
 }
 
 export const focusFirst = async (): Promise<void> => {
-  await Rpc.invoke('Explorer.focusFirst')
+  await RendererWorker.invoke('Explorer.focusFirst')
 }
 
 export const removeDirent = async (): Promise<void> => {
-  await Rpc.invoke('Explorer.removeDirent')
+  await RendererWorker.invoke('Explorer.removeDirent')
 }
 
 export const expandRecursively = async (): Promise<void> => {
-  await Rpc.invoke('Explorer.expandRecursively')
+  await RendererWorker.invoke('Explorer.expandRecursively')
 }
 
 export const newFile = async (): Promise<void> => {
-  await Rpc.invoke('Explorer.newFile')
+  await RendererWorker.invoke('Explorer.newFile')
 }
 
 export const newFolder = async (): Promise<void> => {
-  await Rpc.invoke('Explorer.newFolder')
+  await RendererWorker.invoke('Explorer.newFolder')
 }
 
 export const handleClick = async (index: number): Promise<void> => {
-  await Rpc.invoke('Explorer.handleClick', index)
+  await RendererWorker.invoke('Explorer.handleClick', index)
 }
 
 export const handleClickAt = async (
@@ -93,66 +98,66 @@ export const handleClickAt = async (
   y: number,
 ): Promise<void> => {
   // @ts-ignore
-  await Rpc.invoke('Explorer.handleClickAt', preventDefault, button, ctrlKey, shiftKey, x, y)
+  await RendererWorker.invoke('Explorer.handleClickAt', preventDefault, button, ctrlKey, shiftKey, x, y)
 }
 
 export const handleDrop = async (x: number, y: number, fileIds: readonly number[], fileList: FileList | readonly File[]): Promise<void> => {
   // @ts-ignore
-  await Rpc.invoke('Explorer.handleDrop', x, y, fileIds, fileIds)
+  await RendererWorker.invoke('Explorer.handleDrop', x, y, fileIds, fileIds)
 }
 
 export const rename = async (): Promise<void> => {
-  await Rpc.invoke('Explorer.renameDirent')
+  await RendererWorker.invoke('Explorer.renameDirent')
 }
 
 export const selectAll = async (): Promise<void> => {
-  await Rpc.invoke('Explorer.selectAll')
+  await RendererWorker.invoke('Explorer.selectAll')
 }
 
 export const renameDirent = async (): Promise<void> => {
-  await Rpc.invoke('Explorer.renameDirent')
+  await RendererWorker.invoke('Explorer.renameDirent')
 }
 
 export const cancelEdit = async (): Promise<void> => {
-  await Rpc.invoke('Explorer.cancelEdit')
+  await RendererWorker.invoke('Explorer.cancelEdit')
 }
 
 export const acceptEdit = async (): Promise<void> => {
-  await Rpc.invoke('Explorer.acceptEdit')
+  await RendererWorker.invoke('Explorer.acceptEdit')
 }
 
 export const updateEditingValue = async (value: string): Promise<void> => {
-  await Rpc.invoke('Explorer.updateEditingValue', value)
+  await RendererWorker.invoke('Explorer.updateEditingValue', value)
 }
 
 export const expandAll = async (): Promise<void> => {
-  await Rpc.invoke('Explorer.expandAll')
+  await RendererWorker.invoke('Explorer.expandAll')
 }
 
 export const handleDragOver = async (x: number, y: number): Promise<void> => {
-  await Rpc.invoke('Explorer.handleDragOver', x, y)
+  await RendererWorker.invoke('Explorer.handleDragOver', x, y)
 }
 
 export const handleCut = async (): Promise<void> => {
   // @ts-ignore
-  await Rpc.invoke('Explorer.handleCut')
+  await RendererWorker.invoke('Explorer.handleCut')
 }
 
 export const handleCopy = async (): Promise<void> => {
   // @ts-ignore
-  await Rpc.invoke('Explorer.handleCopy')
+  await RendererWorker.invoke('Explorer.handleCopy')
 }
 
 export const handlePaste = async (): Promise<void> => {
   // @ts-ignore
-  await Rpc.invoke('Explorer.handlePaste')
+  await RendererWorker.invoke('Explorer.handlePaste')
 }
 
 export const selectIndices = async (indices: readonly number[]): Promise<void> => {
-  await Rpc.invoke('Explorer.selectIndices', indices)
+  await RendererWorker.invoke('Explorer.selectIndices', indices)
 }
 
 export const toggleIndividualSelection = async (index: number): Promise<void> => {
   // @ts-ignore
-  await Rpc.invoke('Explorer.toggleIndividualSelection', index)
+  await RendererWorker.invoke('Explorer.toggleIndividualSelection', index)
 }
