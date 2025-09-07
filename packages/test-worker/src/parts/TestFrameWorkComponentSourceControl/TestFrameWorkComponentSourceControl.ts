33b07c12a2e3458d79de5809b3968436e41f07b3
Le Vivilet <levivilet@pm.me>
2025-09-07 19:28:42 +0200

    feature: add input source property to source control

--- packages/test-worker/src/parts/TestFrameWorkComponentSourceControl/TestFrameWorkComponentSourceControl.ts
+++ packages/test-worker/src/parts/TestFrameWorkComponentSourceControl/TestFrameWorkComponentSourceControl.ts
@@ -1,4 +1,5 @@
 import { RendererWorker } from '@lvce-editor/rpc-registry'
+import * as InputSource from '../InputSource/InputSource.ts'
 
 export const selectIndex = async (index: number): Promise<void> => {
   // @ts-ignore
@@ -10,7 +11,8 @@ export const acceptInput = async (): Promise<void> => {
 }
 
 export const handleInput = async (text: string): Promise<void> => {
-  await RendererWorker.invoke('Source Control.handleInput', text)
+  // @ts-ignore
+  await RendererWorker.invoke('Source Control.handleInput', text, InputSource.Script)
 }
 
 export const handleClickSourceControlButtons = async (index: number, name: string): Promise<void> => {
