f047fb00a475f033a673877b2849726bab1224bb
Le Vivilet <levivilet@pm.me>
2025-09-15 11:52:51 +0200

    test

--- packages/test-worker/test/TestFrameWorkComponentCommand.test.ts
+++ packages/test-worker/test/TestFrameWorkComponentCommand.test.ts
@@ -1,19 +1,16 @@
 import { expect, test } from '@jest/globals'
+import { RendererWorker } from '@lvce-editor/rpc-registry'
 import * as Command from '../src/parts/TestFrameWorkComponentCommand/TestFrameWorkComponentCommand.ts'
-import { createMockRpcWithInvocations } from './test-utils.ts'
 
 test('execute', async () => {
-  const mockRpc = createMockRpcWithInvocations(async (method: string, ...args: readonly any[]) => {
-    if (method === 'Some.command') {
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Some.command'() {
       return 'ok'
-    }
-    throw new Error(`unexpected method ${method}`)
+    },
   })
 
   const result: any = await Command.execute('Some.command', 1, 2)
 
   expect(result).toBe('ok')
-  expect(mockRpc.invocations).toEqual([
-    ['Some.command', 1, 2]
-  ])
+  expect(mockRpc.invocations).toEqual([['Some.command', 1, 2]])
 })
