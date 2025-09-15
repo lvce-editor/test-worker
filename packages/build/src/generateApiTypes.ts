c4e9f6a411bccf113cf22cad9430d4a7e3f55f0d
Le Vivilet <levivilet@pm.me>
2025-09-15 16:22:37 +0200

    feature: generate types in parallel

--- packages/build/src/generateApiTypes.ts
+++ packages/build/src/generateApiTypes.ts
@@ -7,18 +7,21 @@ import { getActualApiTypesContent } from './getActualApiTypesContent.ts'
 export const generateApiTypes = async (): Promise<void> => {
   const ext = process.platform === 'win32' ? '' : ''
   const bundleGeneratorPath = join(root, 'packages', 'build', 'node_modules', '.bin', 'dts-bundle-generator' + ext)
-  await execa(bundleGeneratorPath, ['-o', '../../.tmp/api-types/api.d.ts', 'src/parts/TestFrameWorkComponent/TestFrameWorkComponent.ts'], {
-    cwd: join(root, 'packages', 'test-worker'),
-    reject: false,
-  })
-  await execa(bundleGeneratorPath, ['-o', '../../.tmp/api-types/expect.d.ts', 'src/parts/Expect/Expect.ts'], {
-    cwd: join(root, 'packages', 'test-worker'),
-    reject: false,
-  })
-  await execa(bundleGeneratorPath, ['-o', '../../.tmp/api-types/locator.d.ts', 'src/parts/Locator/Locator.ts'], {
-    cwd: join(root, 'packages', 'test-worker'),
-    reject: false,
-  })
+
+  await Promise.all([
+    execa(bundleGeneratorPath, ['-o', '../../.tmp/api-types/api.d.ts', 'src/parts/TestFrameWorkComponent/TestFrameWorkComponent.ts'], {
+      cwd: join(root, 'packages', 'test-worker'),
+      reject: false,
+    }),
+    execa(bundleGeneratorPath, ['-o', '../../.tmp/api-types/expect.d.ts', 'src/parts/Expect/Expect.ts'], {
+      cwd: join(root, 'packages', 'test-worker'),
+      reject: false,
+    }),
+    execa(bundleGeneratorPath, ['-o', '../../.tmp/api-types/locator.d.ts', 'src/parts/Locator/Locator.ts'], {
+      cwd: join(root, 'packages', 'test-worker'),
+      reject: false,
+    }),
+  ])
   const content = await readFile(join(root, '.tmp', 'api-types', 'api.d.ts'), 'utf8')
   const actual = getActualApiTypesContent(content)
   await writeFile(join(root, '.tmp', 'dist', 'dist', 'api.d.ts'), actual)
