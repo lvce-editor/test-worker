27c71fb312e65f387d52c57f2a9ebb54d1ca232b
Le Vivilet <levivilet@pm.me>
2025-09-15 19:12:36 +0200

    feature: add editor worker rpc test (#507)

---
+++ packages/test-worker/test/LaunchEditorWorkerRpc.test.ts
@@ -0,0 +1,46 @@
+import { expect, test } from '@jest/globals'
+import { RendererWorker } from '@lvce-editor/rpc-registry'
+import { launchEditorWorkerRpc } from '../src/parts/LaunchEditorWorkerRpc/LaunchEditorWorkerRpc.ts'
+
+test('launchEditorWorkerRpc creates RPC with correct configuration', async () => {
+  const mockRpc = RendererWorker.registerMockRpc({
+    'SendMessagePortToExtensionHostWorker.sendMessagePortToEditorWorker'() {
+      return undefined
+    },
+  })
+
+  const rpc = await launchEditorWorkerRpc()
+
+  expect(rpc).toBeDefined()
+  expect(typeof rpc.invoke).toBe('function')
+  expect(mockRpc.invocations).toHaveLength(1)
+  expect(mockRpc.invocations[0][0]).toBe('SendMessagePortToExtensionHostWorker.sendMessagePortToEditorWorker')
+})
+
+test('launchEditorWorkerRpc returns RPC with empty commandMap', async () => {
+  RendererWorker.registerMockRpc({
+    'SendMessagePortToExtensionHostWorker.sendMessagePortToEditorWorker'() {
+      return undefined
+    },
+  })
+
+  const rpc = await launchEditorWorkerRpc()
+
+  expect(rpc).toBeDefined()
+  expect(typeof rpc.invoke).toBe('function')
+})
+
+test('launchEditorWorkerRpc calls sendMessagePortToEditorWorker with correct parameters', async () => {
+  const mockRpc = RendererWorker.registerMockRpc({
+    'SendMessagePortToExtensionHostWorker.sendMessagePortToEditorWorker'() {
+      return undefined
+    },
+  })
+
+  await launchEditorWorkerRpc()
+
+  expect(mockRpc.invocations).toHaveLength(1)
+  expect(mockRpc.invocations[0][0]).toBe('SendMessagePortToExtensionHostWorker.sendMessagePortToEditorWorker')
+  expect(mockRpc.invocations[0][1]).toBeDefined() // MessagePort
+  expect(mockRpc.invocations[0][2]).toBe('HandleMessagePort.handleMessagePort') // RpcId.TestWorker
+})
