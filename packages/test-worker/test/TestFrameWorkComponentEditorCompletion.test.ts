752b42cc66608f83abd7814a99468d6c17d40802
Le Vivilet <levivilet@pm.me>
2025-09-15 21:39:02 +0200

    feature: simplify completion tests

--- packages/test-worker/test/TestFrameWorkComponentEditorCompletion.test.ts
+++ packages/test-worker/test/TestFrameWorkComponentEditorCompletion.test.ts
@@ -1,48 +1,51 @@
-import { expect, jest, test } from '@jest/globals'
-import { MockRpc } from '@lvce-editor/rpc'
+import { expect, test } from '@jest/globals'
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 import * as EditorCompletion from '../src/parts/TestFrameWorkComponentEditorCompletion/TestFrameWorkComponentEditorCompletion.ts'
 
 test('selectIndex', async () => {
-  const mockInvoke = jest.fn()
-  const mockRpc = MockRpc.create({ commandMap: {}, invoke: mockInvoke })
-  RendererWorker.set(mockRpc)
+  const mockRpc = RendererWorker.registerMockRpc({
+    'EditorCompletion.selectIndex'() {
+      return undefined
+    },
+  })
 
   await EditorCompletion.selectIndex(3)
 
-  expect(mockInvoke).toHaveBeenCalledTimes(1)
-  expect(mockInvoke).toHaveBeenCalledWith('EditorCompletion.selectIndex', 3)
+  expect(mockRpc.invocations).toEqual([['EditorCompletion.selectIndex', 3]])
 })
 
 test('selectCurrentIndex', async () => {
-  const mockInvoke = jest.fn()
-  const mockRpc = MockRpc.create({ commandMap: {}, invoke: mockInvoke })
-  RendererWorker.set(mockRpc)
+  const mockRpc = RendererWorker.registerMockRpc({
+    'EditorCompletion.selectCurrentIndex'() {
+      return undefined
+    },
+  })
 
   await EditorCompletion.selectCurrentIndex()
 
-  expect(mockInvoke).toHaveBeenCalledTimes(1)
-  expect(mockInvoke).toHaveBeenCalledWith('EditorCompletion.selectCurrentIndex')
+  expect(mockRpc.invocations).toEqual([['EditorCompletion.selectCurrentIndex']])
 })
 
 test('close', async () => {
-  const mockInvoke = jest.fn()
-  const mockRpc = MockRpc.create({ commandMap: {}, invoke: mockInvoke })
-  RendererWorker.set(mockRpc)
+  const mockRpc = RendererWorker.registerMockRpc({
+    'EditorCompletion.close'() {
+      return undefined
+    },
+  })
 
   await EditorCompletion.close()
 
-  expect(mockInvoke).toHaveBeenCalledTimes(1)
-  expect(mockInvoke).toHaveBeenCalledWith('EditorCompletion.close')
+  expect(mockRpc.invocations).toEqual([['EditorCompletion.close']])
 })
 
 test('handleWheel', async () => {
-  const mockInvoke = jest.fn()
-  const mockRpc = MockRpc.create({ commandMap: {}, invoke: mockInvoke })
-  RendererWorker.set(mockRpc)
+  const mockRpc = RendererWorker.registerMockRpc({
+    'EditorCompletion.handleWheel'() {
+      return undefined
+    },
+  })
 
   await EditorCompletion.handleWheel(0, 100)
 
-  expect(mockInvoke).toHaveBeenCalledTimes(1)
-  expect(mockInvoke).toHaveBeenCalledWith('EditorCompletion.handleWheel', 0, 100)
+  expect(mockRpc.invocations).toEqual([['EditorCompletion.handleWheel', 0, 100]])
 })
