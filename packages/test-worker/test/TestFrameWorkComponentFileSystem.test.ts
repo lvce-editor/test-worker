510f1f608a84c940385fd5b78f4618f7bbf40732
Le Vivilet <levivilet@pm.me>
2025-09-15 22:39:43 +0200

    Discard changes to packages/test-worker/test/TestFrameWorkComponentFileSystem.test.ts

--- packages/test-worker/test/TestFrameWorkComponentFileSystem.test.ts
+++ packages/test-worker/test/TestFrameWorkComponentFileSystem.test.ts
@@ -12,8 +12,6 @@ test('writeFile', async () => {
   await FileSystem.writeFile('memfs:///file.txt', 'content')
 
   expect(mockRpc.invocations).toEqual([['FileSystem.writeFile', 'memfs:///file.txt', 'content']])
-  
-  await mockRpc.dispose()
 })
 
 test('writeJson', async () => {
