8b29403c350795b45a969ae2e7263a247f546e61
Le Vivilet <levivilet@pm.me>
2025-09-15 21:42:51 +0200

    feature: simplify rpc in tests

--- packages/test-worker/test/TestFrameWorkComponentDeveloper.test.ts
+++ packages/test-worker/test/TestFrameWorkComponentDeveloper.test.ts
@@ -1,56 +1,75 @@
-import { expect, jest, test } from '@jest/globals'
-import { MockRpc } from '@lvce-editor/rpc'
+import { expect, test } from '@jest/globals'
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 import * as Developer from '../src/parts/TestFrameWorkComponentDeveloper/TestFrameWorkComponentDeveloper.ts'
 
 test('openIframeInspector', async () => {
-  const mockInvoke = jest.fn()
-  const mockRpc = MockRpc.create({ commandMap: {}, invoke: mockInvoke })
-  RendererWorker.set(mockRpc)
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Developer.openIframeInspector'() {
+      return undefined
+    },
+  })
   await Developer.openIframeInspector()
-  expect(mockInvoke).toHaveBeenCalledWith('Developer.openIframeInspector')
+  expect(mockRpc.invocations).toEqual([['Developer.openIframeInspector']])
 })
 
 test('openCacheFolder', async () => {
-  const mockInvoke = jest.fn()
-  const mockRpc = MockRpc.create({ commandMap: {}, invoke: mockInvoke })
-  RendererWorker.set(mockRpc)
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Developer.openCacheFolder'() {
+      return undefined
+    },
+  })
   await Developer.openCacheFolder()
-  expect(mockInvoke).toHaveBeenCalledWith('Developer.openCacheFolder')
+  expect(mockRpc.invocations).toEqual([['Developer.openCacheFolder']])
 })
 
 test('openConfigFolder', async () => {
-  const mockInvoke = jest.fn()
-  const mockRpc = MockRpc.create({ commandMap: {}, invoke: mockInvoke })
-  RendererWorker.set(mockRpc)
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Developer.openConfigFolder'() {
+      return undefined
+    },
+  })
   await Developer.openConfigFolder()
-  expect(mockInvoke).toHaveBeenCalledWith('Developer.openConfigFolder')
+  expect(mockRpc.invocations).toEqual([['Developer.openConfigFolder']])
 })
 
 test('openLogsFolder', async () => {
-  const mockInvoke = jest.fn()
-  const mockRpc = MockRpc.create({ commandMap: {}, invoke: mockInvoke })
-  RendererWorker.set(mockRpc)
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Developer.openLogsFolder'() {
+      return undefined
+    },
+  })
   await Developer.openLogsFolder()
-  expect(mockInvoke).toHaveBeenCalledWith('Developer.openLogsFolder')
+  expect(mockRpc.invocations).toEqual([['Developer.openLogsFolder']])
 })
 
 test('openProcessExplorer', async () => {
-  const mockInvoke = jest.fn()
-  const mockRpc = MockRpc.create({ commandMap: {}, invoke: mockInvoke })
-  RendererWorker.set(mockRpc)
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Developer.openProcessExplorer'() {
+      return undefined
+    },
+  })
   await Developer.openProcessExplorer()
-  expect(mockInvoke).toHaveBeenCalledWith('Developer.openProcessExplorer')
+  expect(mockRpc.invocations).toEqual([['Developer.openProcessExplorer']])
 })
 
 test('reloadColorTheme and reloadIconTheme and toggleDeveloperTools', async () => {
-  const mockInvoke = jest.fn()
-  const mockRpc = MockRpc.create({ commandMap: {}, invoke: mockInvoke })
-  RendererWorker.set(mockRpc)
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Developer.reloadColorTheme'() {
+      return undefined
+    },
+    'Developer.reloadIconTheme'() {
+      return undefined
+    },
+    'Developer.toggleDeveloperTools'() {
+      return undefined
+    },
+  })
   await Developer.reloadColorTheme()
   await Developer.reloadIconTheme()
   await Developer.toggleDeveloperTools()
-  expect(mockInvoke).toHaveBeenCalledWith('Developer.reloadColorTheme')
-  expect(mockInvoke).toHaveBeenCalledWith('Developer.reloadIconTheme')
-  expect(mockInvoke).toHaveBeenCalledWith('Developer.toggleDeveloperTools')
+  expect(mockRpc.invocations).toEqual([
+    ['Developer.reloadColorTheme'],
+    ['Developer.reloadIconTheme'],
+    ['Developer.toggleDeveloperTools']
+  ])
 })
