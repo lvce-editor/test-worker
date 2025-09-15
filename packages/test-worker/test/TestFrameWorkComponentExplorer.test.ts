59322e03b67b7883f8d5f9793fa27c0bbe9d516e
Le Vivilet <levivilet@pm.me>
2025-09-15 19:28:39 +0200

    feature: simplify explorer tests

--- packages/test-worker/test/TestFrameWorkComponentExplorer.test.ts
+++ packages/test-worker/test/TestFrameWorkComponentExplorer.test.ts
@@ -1,498 +1,427 @@
-import { test, jest, expect } from '@jest/globals'
-import { MockRpc } from '@lvce-editor/rpc'
+import { test, expect } from '@jest/globals'
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 import * as Explorer from '../src/parts/TestFrameWorkComponentExplorer/TestFrameWorkComponentExplorer.ts'
 
 test('openContextMenu', async () => {
-  const mockInvoke = jest.fn()
-  const mockRpc = MockRpc.create({
-    commandMap: {},
-    invoke: mockInvoke,
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Explorer.handleContextMenuKeyboard'() {
+      return undefined
+    },
   })
-  RendererWorker.set(mockRpc)
 
   await Explorer.openContextMenu(5)
 
-  expect(mockInvoke).toHaveBeenCalledTimes(1)
-  expect(mockInvoke).toHaveBeenCalledWith('Explorer.handleContextMenuKeyboard', 5)
+  expect(mockRpc.invocations).toEqual([['Explorer.handleContextMenuKeyboard', 5]])
 })
 
 test('handleDragLeave', async () => {
-  const mockInvoke = jest.fn()
-  const mockRpc = MockRpc.create({
-    commandMap: {},
-    invoke: mockInvoke,
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Explorer.handleDragLeave'() {
+      return undefined
+    },
   })
-  RendererWorker.set(mockRpc)
 
   await Explorer.handleDragLeave()
 
-  expect(mockInvoke).toHaveBeenCalledTimes(1)
-  expect(mockInvoke).toHaveBeenCalledWith('Explorer.handleDragLeave')
+  expect(mockRpc.invocations).toEqual([['Explorer.handleDragLeave']])
 })
 
 test('handleBlur', async () => {
-  const mockInvoke = jest.fn()
-  const mockRpc = MockRpc.create({
-    commandMap: {},
-    invoke: mockInvoke,
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Explorer.handleBlur'() {
+      return undefined
+    },
   })
-  RendererWorker.set(mockRpc)
 
   await Explorer.handleBlur()
 
-  expect(mockInvoke).toHaveBeenCalledTimes(1)
-  expect(mockInvoke).toHaveBeenCalledWith('Explorer.handleBlur')
+  expect(mockRpc.invocations).toEqual([['Explorer.handleBlur']])
 })
 
 test('handleInputBlur', async () => {
-  const mockInvoke = jest.fn()
-  const mockRpc = MockRpc.create({
-    commandMap: {},
-    invoke: mockInvoke,
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Explorer.handleInputBlur'() {
+      return undefined
+    },
   })
-  RendererWorker.set(mockRpc)
 
   await Explorer.handleInputBlur()
 
-  expect(mockInvoke).toHaveBeenCalledTimes(1)
-  expect(mockInvoke).toHaveBeenCalledWith('Explorer.handleInputBlur')
+  expect(mockRpc.invocations).toEqual([['Explorer.handleInputBlur']])
 })
 
 test('focus', async () => {
-  const mockInvoke = jest.fn()
-  const mockRpc = MockRpc.create({
-    commandMap: {},
-    invoke: mockInvoke,
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Explorer.focusIndex'() {
+      return undefined
+    },
   })
-  RendererWorker.set(mockRpc)
 
   await Explorer.focus()
 
-  expect(mockInvoke).toHaveBeenCalledTimes(1)
-  expect(mockInvoke).toHaveBeenCalledWith('Explorer.focusIndex', -1)
+  expect(mockRpc.invocations).toEqual([['Explorer.focusIndex', -1]])
 })
 
 test('focusNext', async () => {
-  const mockInvoke = jest.fn()
-  const mockRpc = MockRpc.create({
-    commandMap: {},
-    invoke: mockInvoke,
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Explorer.focusNext'() {
+      return undefined
+    },
   })
-  RendererWorker.set(mockRpc)
 
   await Explorer.focusNext()
 
-  expect(mockInvoke).toHaveBeenCalledTimes(1)
-  expect(mockInvoke).toHaveBeenCalledWith('Explorer.focusNext')
+  expect(mockRpc.invocations).toEqual([['Explorer.focusNext']])
 })
 
 test('selectUp', async () => {
-  const mockInvoke = jest.fn()
-  const mockRpc = MockRpc.create({
-    commandMap: {},
-    invoke: mockInvoke,
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Explorer.selectUp'() {
+      return undefined
+    },
   })
-  RendererWorker.set(mockRpc)
 
   await Explorer.selectUp()
 
-  expect(mockInvoke).toHaveBeenCalledTimes(1)
-  expect(mockInvoke).toHaveBeenCalledWith('Explorer.selectUp')
+  expect(mockRpc.invocations).toEqual([['Explorer.selectUp']])
 })
 
 test('handleDragOverIndex', async () => {
-  const mockInvoke = jest.fn()
-  const mockRpc = MockRpc.create({
-    commandMap: {},
-    invoke: mockInvoke,
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Explorer.handleDragOverIndex'() {
+      return undefined
+    },
   })
-  RendererWorker.set(mockRpc)
 
   await Explorer.handleDragOverIndex(3)
 
-  expect(mockInvoke).toHaveBeenCalledTimes(1)
-  expect(mockInvoke).toHaveBeenCalledWith('Explorer.handleDragOverIndex', 3)
+  expect(mockRpc.invocations).toEqual([['Explorer.handleDragOverIndex', 3]])
 })
 
 test('selectDown', async () => {
-  const mockInvoke = jest.fn()
-  const mockRpc = MockRpc.create({
-    commandMap: {},
-    invoke: mockInvoke,
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Explorer.selectDown'() {
+      return undefined
+    },
   })
-  RendererWorker.set(mockRpc)
 
   await Explorer.selectDown()
 
-  expect(mockInvoke).toHaveBeenCalledTimes(1)
-  expect(mockInvoke).toHaveBeenCalledWith('Explorer.selectDown')
+  expect(mockRpc.invocations).toEqual([['Explorer.selectDown']])
 })
 
 test('refresh', async () => {
-  const mockInvoke = jest.fn()
-  const mockRpc = MockRpc.create({
-    commandMap: {},
-    invoke: mockInvoke,
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Explorer.refresh'() {
+      return undefined
+    },
   })
-  RendererWorker.set(mockRpc)
 
   await Explorer.refresh()
 
-  expect(mockInvoke).toHaveBeenCalledTimes(1)
-  expect(mockInvoke).toHaveBeenCalledWith('Explorer.refresh')
+  expect(mockRpc.invocations).toEqual([['Explorer.refresh']])
 })
 
 test('focusIndex', async () => {
-  const mockInvoke = jest.fn()
-  const mockRpc = MockRpc.create({
-    commandMap: {},
-    invoke: mockInvoke,
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Explorer.focusIndex'() {
+      return undefined
+    },
   })
-  RendererWorker.set(mockRpc)
 
   await Explorer.focusIndex(7)
 
-  expect(mockInvoke).toHaveBeenCalledTimes(1)
-  expect(mockInvoke).toHaveBeenCalledWith('Explorer.focusIndex', 7)
+  expect(mockRpc.invocations).toEqual([['Explorer.focusIndex', 7]])
 })
 
 test('clickCurrent', async () => {
-  const mockInvoke = jest.fn()
-  const mockRpc = MockRpc.create({
-    commandMap: {},
-    invoke: mockInvoke,
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Explorer.handleClickCurrent'() {
+      return undefined
+    },
   })
-  RendererWorker.set(mockRpc)
 
   await Explorer.clickCurrent()
 
-  expect(mockInvoke).toHaveBeenCalledTimes(1)
-  expect(mockInvoke).toHaveBeenCalledWith('Explorer.handleClickCurrent')
+  expect(mockRpc.invocations).toEqual([['Explorer.handleClickCurrent']])
 })
 
 test('handleArrowLeft', async () => {
-  const mockInvoke = jest.fn()
-  const mockRpc = MockRpc.create({
-    commandMap: {},
-    invoke: mockInvoke,
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Explorer.handleArrowLeft'() {
+      return undefined
+    },
   })
-  RendererWorker.set(mockRpc)
 
   await Explorer.handleArrowLeft()
 
-  expect(mockInvoke).toHaveBeenCalledTimes(1)
-  expect(mockInvoke).toHaveBeenCalledWith('Explorer.handleArrowLeft')
+  expect(mockRpc.invocations).toEqual([['Explorer.handleArrowLeft']])
 })
 
 test('focusLast', async () => {
-  const mockInvoke = jest.fn()
-  const mockRpc = MockRpc.create({
-    commandMap: {},
-    invoke: mockInvoke,
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Explorer.focusLast'() {
+      return undefined
+    },
   })
-  RendererWorker.set(mockRpc)
 
   await Explorer.focusLast()
 
-  expect(mockInvoke).toHaveBeenCalledTimes(1)
-  expect(mockInvoke).toHaveBeenCalledWith('Explorer.focusLast')
+  expect(mockRpc.invocations).toEqual([['Explorer.focusLast']])
 })
 
 test('focusFirst', async () => {
-  const mockInvoke = jest.fn()
-  const mockRpc = MockRpc.create({
-    commandMap: {},
-    invoke: mockInvoke,
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Explorer.focusFirst'() {
+      return undefined
+    },
   })
-  RendererWorker.set(mockRpc)
 
   await Explorer.focusFirst()
 
-  expect(mockInvoke).toHaveBeenCalledTimes(1)
-  expect(mockInvoke).toHaveBeenCalledWith('Explorer.focusFirst')
+  expect(mockRpc.invocations).toEqual([['Explorer.focusFirst']])
 })
 
 test('removeDirent', async () => {
-  const mockInvoke = jest.fn()
-  const mockRpc = MockRpc.create({
-    commandMap: {},
-    invoke: mockInvoke,
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Explorer.removeDirent'() {
+      return undefined
+    },
   })
-  RendererWorker.set(mockRpc)
 
   await Explorer.removeDirent()
 
-  expect(mockInvoke).toHaveBeenCalledTimes(1)
-  expect(mockInvoke).toHaveBeenCalledWith('Explorer.removeDirent')
+  expect(mockRpc.invocations).toEqual([['Explorer.removeDirent']])
 })
 
 test('expandRecursively', async () => {
-  const mockInvoke = jest.fn()
-  const mockRpc = MockRpc.create({
-    commandMap: {},
-    invoke: mockInvoke,
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Explorer.expandRecursively'() {
+      return undefined
+    },
   })
-  RendererWorker.set(mockRpc)
 
   await Explorer.expandRecursively()
 
-  expect(mockInvoke).toHaveBeenCalledTimes(1)
-  expect(mockInvoke).toHaveBeenCalledWith('Explorer.expandRecursively')
+  expect(mockRpc.invocations).toEqual([['Explorer.expandRecursively']])
 })
 
 test('newFile', async () => {
-  const mockInvoke = jest.fn()
-  const mockRpc = MockRpc.create({
-    commandMap: {},
-    invoke: mockInvoke,
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Explorer.newFile'() {
+      return undefined
+    },
   })
-  RendererWorker.set(mockRpc)
 
   await Explorer.newFile()
 
-  expect(mockInvoke).toHaveBeenCalledTimes(1)
-  expect(mockInvoke).toHaveBeenCalledWith('Explorer.newFile')
+  expect(mockRpc.invocations).toEqual([['Explorer.newFile']])
 })
 
 test('newFolder', async () => {
-  const mockInvoke = jest.fn()
-  const mockRpc = MockRpc.create({
-    commandMap: {},
-    invoke: mockInvoke,
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Explorer.newFolder'() {
+      return undefined
+    },
   })
-  RendererWorker.set(mockRpc)
 
   await Explorer.newFolder()
 
-  expect(mockInvoke).toHaveBeenCalledTimes(1)
-  expect(mockInvoke).toHaveBeenCalledWith('Explorer.newFolder')
+  expect(mockRpc.invocations).toEqual([['Explorer.newFolder']])
 })
 
 test('handleClick', async () => {
-  const mockInvoke = jest.fn()
-  const mockRpc = MockRpc.create({
-    commandMap: {},
-    invoke: mockInvoke,
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Explorer.handleClick'() {
+      return undefined
+    },
   })
-  RendererWorker.set(mockRpc)
 
   await Explorer.handleClick(4)
 
-  expect(mockInvoke).toHaveBeenCalledTimes(1)
-  expect(mockInvoke).toHaveBeenCalledWith('Explorer.handleClick', 4)
+  expect(mockRpc.invocations).toEqual([['Explorer.handleClick', 4]])
 })
 
 test('handleClickAt', async () => {
-  const mockInvoke = jest.fn()
-  const mockRpc = MockRpc.create({
-    commandMap: {},
-    invoke: mockInvoke,
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Explorer.handleClickAt'() {
+      return undefined
+    },
   })
-  RendererWorker.set(mockRpc)
 
   await Explorer.handleClickAt(true, 1, false, true, 100, 200)
 
-  expect(mockInvoke).toHaveBeenCalledTimes(1)
-  expect(mockInvoke).toHaveBeenCalledWith('Explorer.handleClickAt', true, 1, false, true, 100, 200)
+  expect(mockRpc.invocations).toEqual([['Explorer.handleClickAt', true, 1, false, true, 100, 200]])
 })
 
 test('handleDrop', async () => {
-  const mockInvoke = jest.fn()
-  const mockRpc = MockRpc.create({
-    commandMap: {},
-    invoke: mockInvoke,
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Explorer.handleDrop'() {
+      return undefined
+    },
   })
-  RendererWorker.set(mockRpc)
 
   const fileIds = [1, 2, 3]
   const fileList = [] as readonly File[]
 
   await Explorer.handleDrop(150, 250, fileIds, fileList)
 
-  expect(mockInvoke).toHaveBeenCalledTimes(1)
-  expect(mockInvoke).toHaveBeenCalledWith('Explorer.handleDrop', 150, 250, fileIds, fileIds)
+  expect(mockRpc.invocations).toEqual([['Explorer.handleDrop', 150, 250, fileIds, fileIds]])
 })
 
 test('rename', async () => {
-  const mockInvoke = jest.fn()
-  const mockRpc = MockRpc.create({
-    commandMap: {},
-    invoke: mockInvoke,
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Explorer.renameDirent'() {
+      return undefined
+    },
   })
-  RendererWorker.set(mockRpc)
 
   await Explorer.rename()
 
-  expect(mockInvoke).toHaveBeenCalledTimes(1)
-  expect(mockInvoke).toHaveBeenCalledWith('Explorer.renameDirent')
+  expect(mockRpc.invocations).toEqual([['Explorer.renameDirent']])
 })
 
 test('selectAll', async () => {
-  const mockInvoke = jest.fn()
-  const mockRpc = MockRpc.create({
-    commandMap: {},
-    invoke: mockInvoke,
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Explorer.selectAll'() {
+      return undefined
+    },
   })
-  RendererWorker.set(mockRpc)
 
   await Explorer.selectAll()
 
-  expect(mockInvoke).toHaveBeenCalledTimes(1)
-  expect(mockInvoke).toHaveBeenCalledWith('Explorer.selectAll')
+  expect(mockRpc.invocations).toEqual([['Explorer.selectAll']])
 })
 
 test('renameDirent', async () => {
-  const mockInvoke = jest.fn()
-  const mockRpc = MockRpc.create({
-    commandMap: {},
-    invoke: mockInvoke,
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Explorer.renameDirent'() {
+      return undefined
+    },
   })
-  RendererWorker.set(mockRpc)
 
   await Explorer.renameDirent()
 
-  expect(mockInvoke).toHaveBeenCalledTimes(1)
-  expect(mockInvoke).toHaveBeenCalledWith('Explorer.renameDirent')
+  expect(mockRpc.invocations).toEqual([['Explorer.renameDirent']])
 })
 
 test('cancelEdit', async () => {
-  const mockInvoke = jest.fn()
-  const mockRpc = MockRpc.create({
-    commandMap: {},
-    invoke: mockInvoke,
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Explorer.cancelEdit'() {
+      return undefined
+    },
   })
-  RendererWorker.set(mockRpc)
 
   await Explorer.cancelEdit()
 
-  expect(mockInvoke).toHaveBeenCalledTimes(1)
-  expect(mockInvoke).toHaveBeenCalledWith('Explorer.cancelEdit')
+  expect(mockRpc.invocations).toEqual([['Explorer.cancelEdit']])
 })
 
 test('acceptEdit', async () => {
-  const mockInvoke = jest.fn()
-  const mockRpc = MockRpc.create({
-    commandMap: {},
-    invoke: mockInvoke,
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Explorer.acceptEdit'() {
+      return undefined
+    },
   })
-  RendererWorker.set(mockRpc)
 
   await Explorer.acceptEdit()
 
-  expect(mockInvoke).toHaveBeenCalledTimes(1)
-  expect(mockInvoke).toHaveBeenCalledWith('Explorer.acceptEdit')
+  expect(mockRpc.invocations).toEqual([['Explorer.acceptEdit']])
 })
 
 test('updateEditingValue', async () => {
-  const mockInvoke = jest.fn()
-  const mockRpc = MockRpc.create({
-    commandMap: {},
-    invoke: mockInvoke,
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Explorer.updateEditingValue'() {
+      return undefined
+    },
   })
-  RendererWorker.set(mockRpc)
 
   await Explorer.updateEditingValue('new-file-name')
 
-  expect(mockInvoke).toHaveBeenCalledTimes(1)
-  expect(mockInvoke).toHaveBeenCalledWith('Explorer.updateEditingValue', 'new-file-name')
+  expect(mockRpc.invocations).toEqual([['Explorer.updateEditingValue', 'new-file-name']])
 })
 
 test('expandAll', async () => {
-  const mockInvoke = jest.fn()
-  const mockRpc = MockRpc.create({
-    commandMap: {},
-    invoke: mockInvoke,
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Explorer.expandAll'() {
+      return undefined
+    },
   })
-  RendererWorker.set(mockRpc)
 
   await Explorer.expandAll()
 
-  expect(mockInvoke).toHaveBeenCalledTimes(1)
-  expect(mockInvoke).toHaveBeenCalledWith('Explorer.expandAll')
+  expect(mockRpc.invocations).toEqual([['Explorer.expandAll']])
 })
 
 test('handleDragOver', async () => {
-  const mockInvoke = jest.fn()
-  const mockRpc = MockRpc.create({
-    commandMap: {},
-    invoke: mockInvoke,
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Explorer.handleDragOver'() {
+      return undefined
+    },
   })
-  RendererWorker.set(mockRpc)
 
   await Explorer.handleDragOver(120, 180)
 
-  expect(mockInvoke).toHaveBeenCalledTimes(1)
-  expect(mockInvoke).toHaveBeenCalledWith('Explorer.handleDragOver', 120, 180)
+  expect(mockRpc.invocations).toEqual([['Explorer.handleDragOver', 120, 180]])
 })
 
 test('handleCut', async () => {
-  const mockInvoke = jest.fn()
-  const mockRpc = MockRpc.create({
-    commandMap: {},
-    invoke: mockInvoke,
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Explorer.handleCut'() {
+      return undefined
+    },
   })
-  RendererWorker.set(mockRpc)
 
   await Explorer.handleCut()
 
-  expect(mockInvoke).toHaveBeenCalledTimes(1)
-  expect(mockInvoke).toHaveBeenCalledWith('Explorer.handleCut')
+  expect(mockRpc.invocations).toEqual([['Explorer.handleCut']])
 })
 
 test('handleCopy', async () => {
-  const mockInvoke = jest.fn()
-  const mockRpc = MockRpc.create({
-    commandMap: {},
-    invoke: mockInvoke,
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Explorer.handleCopy'() {
+      return undefined
+    },
   })
-  RendererWorker.set(mockRpc)
 
   await Explorer.handleCopy()
 
-  expect(mockInvoke).toHaveBeenCalledTimes(1)
-  expect(mockInvoke).toHaveBeenCalledWith('Explorer.handleCopy')
+  expect(mockRpc.invocations).toEqual([['Explorer.handleCopy']])
 })
 
 test('handlePaste', async () => {
-  const mockInvoke = jest.fn()
-  const mockRpc = MockRpc.create({
-    commandMap: {},
-    invoke: mockInvoke,
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Explorer.handlePaste'() {
+      return undefined
+    },
   })
-  RendererWorker.set(mockRpc)
 
   await Explorer.handlePaste()
 
-  expect(mockInvoke).toHaveBeenCalledTimes(1)
-  expect(mockInvoke).toHaveBeenCalledWith('Explorer.handlePaste')
+  expect(mockRpc.invocations).toEqual([['Explorer.handlePaste']])
 })
 
 test('selectIndices', async () => {
-  const mockInvoke = jest.fn()
-  const mockRpc = MockRpc.create({
-    commandMap: {},
-    invoke: mockInvoke,
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Explorer.selectIndices'() {
+      return undefined
+    },
   })
-  RendererWorker.set(mockRpc)
 
   const indices = [0, 2, 4, 6]
   await Explorer.selectIndices(indices)
 
-  expect(mockInvoke).toHaveBeenCalledTimes(1)
-  expect(mockInvoke).toHaveBeenCalledWith('Explorer.selectIndices', indices)
+  expect(mockRpc.invocations).toEqual([['Explorer.selectIndices', indices]])
 })
 
 test('toggleIndividualSelection', async () => {
-  const mockInvoke = jest.fn()
-  const mockRpc = MockRpc.create({
-    commandMap: {},
-    invoke: mockInvoke,
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Explorer.toggleIndividualSelection'() {
+      return undefined
+    },
   })
-  RendererWorker.set(mockRpc)
 
   await Explorer.toggleIndividualSelection(8)
 
-  expect(mockInvoke).toHaveBeenCalledTimes(1)
-  expect(mockInvoke).toHaveBeenCalledWith('Explorer.toggleIndividualSelection', 8)
+  expect(mockRpc.invocations).toEqual([['Explorer.toggleIndividualSelection', 8]])
 })
