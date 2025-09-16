aff4d988b72cf8961b57f6f72dc440743fc75629
Le Vivilet <levivilet@pm.me>
2025-09-16 10:02:25 +0200

    fix: dev script

--- packages/build/src/dev.ts
+++ packages/build/src/dev.ts
@@ -2,11 +2,12 @@ import { execa } from 'execa'
 import { root } from './root.ts'
 
 const main = async (): Promise<void> => {
-  await execa(`npm`, ['run', 'build:watch'], {
+  execa(`npm`, ['run', 'build:watch'], {
     cwd: root,
     stdio: 'inherit',
   })
-  await execa('node', ['packages/server/node_modules/@lvce-editor/server/bin/server.js', '--test-path=packages/e2e'], {
+
+  execa('node', ['packages/server/node_modules/@lvce-editor/server/bin/server.js', '--test-path=packages/e2e'], {
     cwd: root,
     stdio: 'inherit',
   })
