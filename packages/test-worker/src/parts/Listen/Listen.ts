37a5066e05c09e2517a002efc05e1e55035a1826
Le Vivilet <levivilet@pm.me>
2025-09-21 00:25:56 +0200

    feature: use createLazyRpc helper function (#539)

--- packages/test-worker/src/parts/Listen/Listen.ts
+++ packages/test-worker/src/parts/Listen/Listen.ts
@@ -1,8 +1,11 @@
 import { WebWorkerRpcClient } from '@lvce-editor/rpc'
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 import * as CommandMap from '../CommandMap/CommandMap.ts'
+import * as EditorWorker from '../EditorWorker/EditorWorker.ts'
+import { launchEditorWorkerRpc } from '../LaunchEditorWorkerRpc/LaunchEditorWorkerRpc.ts'
 
 export const listen = async (): Promise<void> => {
+  EditorWorker.setFactory(launchEditorWorkerRpc)
   const rpc = await WebWorkerRpcClient.create({
     commandMap: CommandMap.commandMap,
   })
