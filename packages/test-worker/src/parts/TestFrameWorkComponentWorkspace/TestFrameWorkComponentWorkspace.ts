72f013d5a0712d078326c5485a9af07c764ded7b
Le Vivilet <levivilet@pm.me>
2025-09-20 11:08:07 +0200

    feature: add resolve file url (#535)

--- packages/test-worker/src/parts/TestFrameWorkComponentWorkspace/TestFrameWorkComponentWorkspace.ts
+++ packages/test-worker/src/parts/TestFrameWorkComponentWorkspace/TestFrameWorkComponentWorkspace.ts
@@ -1,5 +1,6 @@
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 import * as TestFrameWorkComponentFileSystem from '../TestFrameWorkComponentFileSystem/TestFrameWorkComponentFileSystem.ts'
+import { toFileUrl } from '../ToFileUrl/ToFileUrl.ts'
 
 export const setPath = async (path: string): Promise<void> => {
   await RendererWorker.invoke('Workspace.setPath', path)
@@ -9,4 +10,14 @@ export const openTmpDir = async (): Promise<string> => {
   const tmpDir = await TestFrameWorkComponentFileSystem.getTmpDir()
   await setPath(tmpDir)
   return tmpDir
+}
+
+export const resolveFileUrl = (url: string): string => {
+  // TODO in web, convert to memfs or fetch url
+  // TODO on web: read filemap for that fixture
+  // else, use filesystem to read the files
+  // TODO covert remote url to file url
+  // then set that as workspace path
+
+  return toFileUrl(url)
 }
