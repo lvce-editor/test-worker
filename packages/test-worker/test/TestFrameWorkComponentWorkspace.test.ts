e1c3e9a9b6c2f4cf934f217ad00ea0c26d1716d2
Le Vivilet <levivilet@pm.me>
2025-09-22 23:38:34 +0200

    lint

--- packages/test-worker/test/TestFrameWorkComponentWorkspace.test.ts
+++ packages/test-worker/test/TestFrameWorkComponentWorkspace.test.ts
@@ -24,6 +24,7 @@ test('openTmpDir sets workspace path and returns it', async () => {
 })
 
 test('resolveFileUrl converts url to file url', async () => {
+  // eslint-disable-next-line @typescript-eslint/no-deprecated
   const result = Workspace.resolveFileUrl('https://example.com/remote/path/to/file')
   expect(result).toBe('file:///path/to/file')
 })
