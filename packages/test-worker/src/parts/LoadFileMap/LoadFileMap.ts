ee193c3db2d10cde881ced3fdfedcc8de4161ebd
Le Vivilet <levivilet@pm.me>
2025-09-22 14:16:34 +0200

    feature: add argument to loadFileMap

--- packages/test-worker/src/parts/LoadFileMap/LoadFileMap.ts
+++ packages/test-worker/src/parts/LoadFileMap/LoadFileMap.ts
@@ -1,10 +1,11 @@
 import type { FileMap } from '../FileMap/FileMap.ts'
 
-export const loadFileMap = async (): Promise<FileMap> => {
-  const response = await fetch('/filemap.json')
+export const loadFileMap = async (fileMapUrl: string): Promise<FileMap> => {
+  const response = await fetch(fileMapUrl)
   if (!response.ok) {
     throw new Error(`Failed to load filemap.json: ${response.status} ${response.statusText}`)
   }
+  // TODO handle json error
   const fileMap: FileMap = await response.json()
   return fileMap
 }
