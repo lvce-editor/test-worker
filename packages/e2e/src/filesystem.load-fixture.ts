a576a9e5f3597585a6d70a6d268c92605267c11b
Le Vivilet <levivilet@pm.me>
2025-09-23 15:28:40 +0200

    tsc

--- packages/e2e/src/filesystem.load-fixture.ts
+++ packages/e2e/src/filesystem.load-fixture.ts
@@ -5,6 +5,7 @@ export const name = 'filesystem.load-fixture'
 export const test: Test = async ({ Workspace, FileSystem, Main, Editor }) => {
   // arrange
   const fixtureUrl = import.meta.resolve('../fixtures/sample.load-fixture')
+  // @ts-ignore
   const workspaceUrl = await FileSystem.loadFixture(fixtureUrl)
   await Workspace.setPath(workspaceUrl)
   const fileUri = `${workspaceUrl}/example-file.txt`
