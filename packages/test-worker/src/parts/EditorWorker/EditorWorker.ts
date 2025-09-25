c8f2a89e1f8b43a9a00f9552a760e6ce0735d5a8
Le Vivilet <levivilet@pm.me>
2025-09-25 20:01:07 +0200

    test

--- packages/test-worker/src/parts/EditorWorker/EditorWorker.ts
+++ packages/test-worker/src/parts/EditorWorker/EditorWorker.ts
@@ -2,6 +2,3 @@ import { createLazyRpc, RpcId } from '@lvce-editor/rpc-registry'
 
 const lazyRpc = createLazyRpc(RpcId.EditorWorker)
 export const { invoke, setFactory } = lazyRpc
-
-// Add registerMockRpc if it exists
-export const registerMockRpc = (lazyRpc as any).registerMockRpc
