b26e5a8f4f5ca944924fd1274452e5ac73838e78
Le Vivilet <levivilet@pm.me>
2025-09-25 19:30:21 +0200

    feature: add diagnostics settings (#572)

--- packages/test-worker/src/parts/TestFrameWorkComponentSettings/TestFrameWorkComponentSettings.ts
+++ packages/test-worker/src/parts/TestFrameWorkComponentSettings/TestFrameWorkComponentSettings.ts
@@ -4,3 +4,13 @@ export const update = (settings: any): Promise<void> => {
   // @ts-ignore
   return Rpc.invoke('Preferences.update', settings)
 }
+
+export const enableDiagnostics = (): Promise<void> => {
+  // @ts-ignore
+  return Rpc.invoke('Preferences.update', { 'editor.diagnostics': true })
+}
+
+export const disableDiagnostics = (): Promise<void> => {
+  // @ts-ignore
+  return Rpc.invoke('Preferences.update', { 'editor.diagnostics': false })
+}
