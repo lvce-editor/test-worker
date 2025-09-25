2e1338057eb4a61459ba4fc779951b0ecaf461c3
Le Vivilet <levivilet@pm.me>
2025-09-25 19:35:30 +0200

    feature: move editor key into separate file (#573)

---
+++ packages/test-worker/src/parts/GetEditorKey/GetEditorKey.ts
@@ -0,0 +1,11 @@
+import * as EditorWorker from '../EditorWorker/EditorWorker.ts'
+
+export const getEditorKey = async (): Promise<number> => {
+  const keys = await EditorWorker.invoke('Editor.getKeys')
+  if (keys.length === 0) {
+    throw new Error(`no editor found`)
+  }
+  const key = keys.at(-1)
+  const numeric = Number.parseInt(key)
+  return numeric
+}
