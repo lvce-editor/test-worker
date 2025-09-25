6e57d94645b000415bf30b8c702db79d943be1bd
Le Vivilet <levivilet@pm.me>
2025-09-25 19:51:43 +0200

    feature: implement areDiagnosticsEqual

--- packages/test-worker/src/parts/AreDiagnosticsEqual/AreDiagnosticsEqual.ts
+++ packages/test-worker/src/parts/AreDiagnosticsEqual/AreDiagnosticsEqual.ts
@@ -5,12 +5,12 @@ export const areDiagnosticsEqual = (actual: readonly Diagnostic[], expected: rea
   if (actual.length !== expected.length) {
     return false
   }
-  
+
   for (let i = 0; i < actual.length; i++) {
     if (!isDiagnosticEqual(actual[i], expected[i])) {
       return false
     }
   }
-  
+
   return true
 }
