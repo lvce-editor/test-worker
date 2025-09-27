73fbcf3be4be7e2f1d9751bc295ed1fc7367d6f6
Le Vivilet <levivilet@pm.me>
2025-09-27 21:46:25 +0200

    feature: add source control show

--- packages/test-worker/src/parts/TestFrameWorkComponentSourceControl/TestFrameWorkComponentSourceControl.ts
+++ packages/test-worker/src/parts/TestFrameWorkComponentSourceControl/TestFrameWorkComponentSourceControl.ts
@@ -16,3 +16,8 @@ export const handleInput = async (text: string): Promise<void> => {
 export const handleClickSourceControlButtons = async (index: number, name: string): Promise<void> => {
   await RendererWorker.invoke('Source Control.handleClickSourceControlButtons', index, name)
 }
+
+export const show = async (): Promise<void> => {
+  // @ts-ignore
+  await RendererWorker.invoke('SideBar.open', 'Source Control')
+}
