32eb8e12a3a18bd58266a2b076ef127b9cc58f13
Le Vivilet <levivilet@pm.me>
2025-10-31 17:32:17 +0100

    feature: add input function (#637)

--- packages/test-worker/src/parts/TestFrameWorkComponentExtensionSearch/TestFrameWorkComponentExtensionSearch.ts
+++ packages/test-worker/src/parts/TestFrameWorkComponentExtensionSearch/TestFrameWorkComponentExtensionSearch.ts
@@ -1,5 +1,11 @@
+import { InputSource } from '@lvce-editor/constants'
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 
 export const open = async (): Promise<void> => {
   await RendererWorker.invoke('SideBar.openViewlet', 'Extensions')
+}
+
+export const handleInput = async (value: string): Promise<void> => {
+  // @ts-ignore
+  await RendererWorker.invoke('Extensions.handleInput', value, InputSource.Script)
 }
