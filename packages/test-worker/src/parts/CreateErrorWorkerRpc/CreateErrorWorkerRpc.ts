a7b033113bb41c96d16292e27d404966a6414734
Le Vivilet <levivilet@pm.me>
2025-10-31 18:29:07 +0100

    feature: simplify error worker rpc (#639)

--- packages/test-worker/src/parts/CreateErrorWorkerRpc/CreateErrorWorkerRpc.ts
+++ packages/test-worker/src/parts/CreateErrorWorkerRpc/CreateErrorWorkerRpc.ts
@@ -1,15 +1,12 @@
-import { type Rpc, PlainMessagePortRpcParent } from '@lvce-editor/rpc'
+import { type Rpc, TransferMessagePortRpcParent } from '@lvce-editor/rpc'
 import { VError } from '@lvce-editor/verror'
-import * as GetPortTuple from '../GetPortTuple/GetPortTuple.ts'
 import { sendMessagePortToErrorWorker } from '../SendMessagePortToErrorWorker/SendMessagePortToErrorWorker.ts'
 
 export const createErrorWorkerRpc = async (): Promise<Rpc> => {
   try {
-    const { port1, port2 } = GetPortTuple.getPortTuple()
-    await sendMessagePortToErrorWorker(port2)
-    const rpc = await PlainMessagePortRpcParent.create({
+    const rpc = await TransferMessagePortRpcParent.create({
       commandMap: {},
-      messagePort: port1,
+      send: sendMessagePortToErrorWorker,
     })
     return rpc
   } catch (error) {
