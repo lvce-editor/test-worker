a59a4276d38efd50d4cc1bba582092a345e05612
Le Vivilet <levivilet@pm.me>
2025-11-07 13:20:49 +0100

    feature: extract getFileUri function

--- packages/test-worker/src/parts/WatchForHotReload/WatchForHotReload.ts
+++ packages/test-worker/src/parts/WatchForHotReload/WatchForHotReload.ts
@@ -1,6 +1,12 @@
 import { PlatformType } from '@lvce-editor/constants'
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 
+const getFileUri = (href: string): string => {
+  const rest = href.slice('/remote'.length)
+  const fileUrl = `file://${rest}`
+  return fileUrl
+}
+
 export const watchForHotReload = async (platform: number, href: string): Promise<void> => {
   if (platform !== PlatformType.Remote) {
     return
@@ -8,8 +14,7 @@ export const watchForHotReload = async (platform: number, href: string): Promise
   if (!href.startsWith('/remote')) {
     return
   }
-  const rest = href.slice('/remote'.length)
-  const fileUrl = `file://${rest}`
+  const fileUrl = getFileUri(href)
   const id = Math.random()
   // @ts-ignore
   await RendererWorker.invoke('FileWatcher.watchFile', id, fileUrl)
