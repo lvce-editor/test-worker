252c3e8f9068dee7791e6f0bd7d29c4aff6ed252
Le Vivilet <levivilet@pm.me>
2026-01-12 20:56:15 +0100

    feature: add new api to select quickPick item and waiting for callback command to be called (#728)
    * feature: add new api to select quickPick item and waiting for callback command to be called
    
    * tsc

--- packages/test-worker/src/parts/TestFrameWorkComponentQuickPick/TestFrameWorkComponentQuickPick.ts
+++ packages/test-worker/src/parts/TestFrameWorkComponentQuickPick/TestFrameWorkComponentQuickPick.ts
@@ -1,4 +1,5 @@
 import { RendererWorker } from '@lvce-editor/rpc-registry'
+import { registerCallbackCommand } from '../Callback/Callback.ts'
 import * as ViewletModuleId from '../ViewletModuleId/ViewletModuleId.ts'
 
 export const open = async (): Promise<void> => {
@@ -68,4 +69,16 @@ export const executeCommand = async (label: string): Promise<void> => {
   await RendererWorker.invoke('QuickPick.handleInput', label, 0)
   // @ts-ignore
   await RendererWorker.invoke('QuickPick.selectItem', label)
+}
+
+interface SelectItem2Options {
+  readonly callbackCommand: string
+  readonly label: string
+}
+
+export const selectItem2 = async ({ callbackCommand, label }: SelectItem2Options): Promise<void> => {
+  const { promise } = await registerCallbackCommand(callbackCommand)
+  // @ts-ignore
+  const outerPromise = RendererWorker.invoke('QuickPick.selectItem', label)
+  await promise
 }
