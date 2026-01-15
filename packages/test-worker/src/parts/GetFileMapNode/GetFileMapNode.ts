6f5ab2680dc74597572a3d2f31fa9f20cf3b87b3
Le Vivilet <levivilet@pm.me>
2026-01-15 22:42:12 +0100

    feature: avoid mutation in getFileMapNode

--- packages/test-worker/src/parts/GetFileMapNode/GetFileMapNode.ts
+++ packages/test-worker/src/parts/GetFileMapNode/GetFileMapNode.ts
@@ -1,24 +1,25 @@
-/* eslint-disable @typescript-eslint/prefer-readonly-parameter-types */
 import { DirentType } from '@lvce-editor/constants'
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 import type { FileMap } from '../FileMap/FileMap.ts'
 import { toFileUrl } from '../ToFileUrl/ToFileUrl.ts'
 
-const getDirents = async (allDirents: string[], fileUrl: string): Promise<void> => {
+const getDirents = async (fileUrl: string): Promise<readonly string[]> => {
+  const allDirents: string[] = []
   const dirents = await RendererWorker.invoke('FileSystem.readDirWithFileTypes', fileUrl)
   for (const dirent of dirents) {
     if (dirent.type === DirentType.Directory) {
-      await getDirents(allDirents, `${fileUrl}/${dirent.name}`)
+      const subDirents = await getDirents(`${fileUrl}/${dirent.name}`)
+      allDirents.push(...subDirents)
     } else {
       allDirents.push(`${fileUrl}/${dirent.name}`)
     }
   }
+  return allDirents
 }
 
 export const getFileMapNode = async (url: string): Promise<FileMap> => {
   const fileUrl = toFileUrl(url)
-  const allFiles: string[] = []
-  await getDirents(allFiles, fileUrl)
+  const allFiles = await getDirents(fileUrl)
   const fileMap = Object.create(null)
   for (const filePath of allFiles) {
     const content = await RendererWorker.invoke(`FileSystem.readFile`, filePath)
