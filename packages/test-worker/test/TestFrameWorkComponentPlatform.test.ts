df57b025407ec98d6f0a675b7bf7c5560cd3b0e9
Le Vivilet <levivilet@pm.me>
2026-01-15 22:19:36 +0100

    feature: simplify platform test

--- packages/test-worker/test/TestFrameWorkComponentPlatform.test.ts
+++ packages/test-worker/test/TestFrameWorkComponentPlatform.test.ts
@@ -1,15 +1,15 @@
-import { expect, jest, test } from '@jest/globals'
-
-jest.unstable_mockModule('../src/parts/PlatformPaths/PlatformPaths.ts', () => ({
-  getNodePath: jest.fn(),
-}))
-
-const PlatformPaths = await import('../src/parts/PlatformPaths/PlatformPaths.ts')
-const Platform = await import('../src/parts/TestFrameWorkComponentPlatform/TestFrameWorkComponentPlatform.ts')
+import { expect, test } from '@jest/globals'
+import { RendererWorker } from '@lvce-editor/rpc-registry'
+import * as Platform from '../src/parts/TestFrameWorkComponentPlatform/TestFrameWorkComponentPlatform.ts'
 
 test('getNodePath delegates to PlatformPaths.getNodePath', async () => {
-  ;(PlatformPaths.getNodePath as any).mockResolvedValue('/usr/bin/node')
+  using mockRpc = RendererWorker.registerMockRpc({
+    'Platform.getNodePath'() {
+      return '/usr/bin/node'
+    },
+  })
+
   const nodePath = await Platform.getNodePath()
-  expect(PlatformPaths.getNodePath).toHaveBeenCalledTimes(1)
   expect(nodePath).toBe('/usr/bin/node')
+  expect(mockRpc.invocations).toEqual([['Platform.getNodePath']])
 })
