df5da317804bf9dd9b7f9924cb8004ccefeb1a68
Le Vivilet <levivilet@pm.me>
2026-01-15 23:24:34 +0100

    feature: pass time to createUrlWithQueryParams, making it easier to test (#774)

--- packages/test-worker/src/parts/CreateUrlWithQueryParameter/CreateUrlWithQueryParameter.ts
+++ packages/test-worker/src/parts/CreateUrlWithQueryParameter/CreateUrlWithQueryParameter.ts
@@ -1,6 +1,6 @@
-export const createUrlWithQueryParameter = (url: string, locationHref: string): string => {
+export const createUrlWithQueryParameter = (url: string, locationHref: string, time: number): string => {
   const parsedUrl = new URL(url, locationHref)
-  parsedUrl.searchParams.set('time', `${Date.now()}`)
+  parsedUrl.searchParams.set('time', `${time}`)
   const string = parsedUrl.toString()
   return string
 }
