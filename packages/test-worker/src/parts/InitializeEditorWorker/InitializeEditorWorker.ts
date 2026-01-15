606944a005b20ef761848d9afc3a55dfc5ede08d
Le Vivilet <levivilet@pm.me>
2026-01-15 20:53:04 +0100

    feature: update whitespace

--- packages/test-worker/src/parts/InitializeEditorWorker/InitializeEditorWorker.ts
+++ packages/test-worker/src/parts/InitializeEditorWorker/InitializeEditorWorker.ts
@@ -4,6 +4,7 @@ import { EditorWorker, RendererWorker, RpcId } from '@lvce-editor/rpc-registry'
 const send = async (port: MessagePort): Promise<void> => {
   await RendererWorker.sendMessagePortToEditorWorker(port, RpcId.TestWorker)
 }
+
 export const initializeEditorWorker = async (): Promise<void> => {
   const rpc = await LazyTransferMessagePortRpcParent.create({
     commandMap: {},
