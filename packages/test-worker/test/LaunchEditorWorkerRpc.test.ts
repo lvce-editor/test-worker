7737f927b1a65288d90d6b562f24825a8f561687
Le Vivilet <levivilet@pm.me>
2026-01-15 21:50:27 +0100

    fix

--- packages/test-worker/test/LaunchEditorWorkerRpc.test.ts
+++ packages/test-worker/test/LaunchEditorWorkerRpc.test.ts
@@ -13,9 +13,9 @@ test('launchEditorWorkerRpc creates RPC with correct configuration', async () =>
 
   expect(rpc).toBeDefined()
   expect(typeof rpc.invoke).toBe('function')
-  expect(mockRpc.invocations).toEqual([[
-    'SendMessagePortToExtensionHostWorker.sendMessagePortToEditorWorker'
-  ]])
+  expect(mockRpc.invocations).toEqual([
+    ['SendMessagePortToExtensionHostWorker.sendMessagePortToEditorWorker', expect.any(MessagePort), 'HandleMessagePort.handleMessagePort', 9001],
+  ])
 
   // Close the RPC connection to prevent Jest warnings
   await rpc.dispose()
@@ -46,11 +46,14 @@ test('launchEditorWorkerRpc calls sendMessagePortToEditorWorker with correct par
 
   const rpc = await launchEditorWorkerRpc()
 
-  expect(mockRpc.invocations).toEqual([[
-    'SendMessagePortToExtensionHostWorker.sendMessagePortToEditorWorker',
-    expect.any(MessagePort), // MessagePort
-    'HandleMessagePort.handleMessagePort' // RpcId.TestWorker
-  ]])
+  expect(mockRpc.invocations).toEqual([
+    [
+      'SendMessagePortToExtensionHostWorker.sendMessagePortToEditorWorker',
+      expect.any(MessagePort), // MessagePort
+      'HandleMessagePort.handleMessagePort',
+      9001,
+    ],
+  ])
 
   // Close the RPC connection to prevent Jest warnings
   await rpc.dispose()
