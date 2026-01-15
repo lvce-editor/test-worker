07a30428809d2de600b3745d5c733e8aecb6de70
Le Vivilet <levivilet@pm.me>
2026-01-15 21:02:31 +0100

    feature: remove unused test (#742)

--- packages/test-worker/test/TestFrameWorkComponentPlatform.test.ts
+++ packages/test-worker/test/TestFrameWorkComponentPlatform.test.ts
@@ -3,12 +3,8 @@ import { expect, jest, test } from '@jest/globals'
 jest.unstable_mockModule('../src/parts/PlatformPaths/PlatformPaths.ts', () => ({
   getNodePath: jest.fn(),
 }))
-jest.unstable_mockModule('../src/parts/IsFirefox/IsFirefox.ts', () => ({
-  getIsFirefox: jest.fn(() => true),
-}))
 
 const PlatformPaths = await import('../src/parts/PlatformPaths/PlatformPaths.ts')
-const IsFirefox = await import('../src/parts/IsFirefox/IsFirefox.ts')
 const Platform = await import('../src/parts/TestFrameWorkComponentPlatform/TestFrameWorkComponentPlatform.ts')
 
 test('getNodePath delegates to PlatformPaths.getNodePath', async () => {
@@ -16,9 +12,4 @@ test('getNodePath delegates to PlatformPaths.getNodePath', async () => {
   const nodePath = await Platform.getNodePath()
   expect(PlatformPaths.getNodePath).toHaveBeenCalledTimes(1)
   expect(nodePath).toBe('/usr/bin/node')
-})
-
-test('isFirefox returns value from IsFirefox.getIsFirefox', () => {
-  expect(Platform.isFirefox()).toBe(true)
-  expect(IsFirefox.getIsFirefox).toHaveBeenCalledTimes(1)
 })
