00aaece89b73cf4e77628891de7c393e28861cce
Le Vivilet <levivilet@pm.me>
2026-01-29 19:49:59 +0100

    fix

--- packages/test-worker/src/parts/HotReloadTest/HotReloadTest.ts
+++ packages/test-worker/src/parts/HotReloadTest/HotReloadTest.ts
@@ -10,15 +10,12 @@ export interface HotReloadTestOptions {
   readonly time: number
 }
 
-<<<<<<< HEAD
-=======
 const doHotReload = async (url: string, platform: number, assetDir: string): Promise<void> => {
   // eslint-disable-next-line no-console
   console.clear()
   await execute(url, platform, assetDir)
 }
 
->>>>>>> origin/main
 export const hotReloadTest = async ({
   clearConsole,
   getLastTestInfoItem,
@@ -26,23 +23,9 @@ export const hotReloadTest = async ({
   locationHref,
   time,
 }: HotReloadTestOptions): Promise<void> => {
-<<<<<<< HEAD
-  if (!hastTestInfoItems()) {
-    return
-  }
-  const last = getLastTestInfoItem()
-  const { assetDir, inProgress, platform, url } = last
-  if (inProgress) {
-    return
-  }
-  const withQueryParameter = createUrlWithQueryParameter(url, locationHref, time)
-  clearConsole()
-  await execute(withQueryParameter, platform, assetDir)
-=======
   const { assetDir, platform, shouldHotReload, url } = getHotReloadArgs({ latestItem: getLastTestInfoItem(), locationHref, time })
   if (!shouldHotReload) {
     return
   }
   await doHotReload(url, platform, assetDir)
->>>>>>> origin/main
 }
