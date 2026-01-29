39d44a60dfe35cddf561fc79826891f3fe937d9c
Le Vivilet <levivilet@pm.me>
2026-01-29 19:18:51 +0100

    feature: try to make hotreloading easier to test by passing arguments

--- packages/test-worker/src/parts/TestInfoCache/TestInfoCache.ts
+++ packages/test-worker/src/parts/TestInfoCache/TestInfoCache.ts
@@ -2,6 +2,13 @@ import type { TestInfoItem } from './TestInfoItem.ts'
 
 export type { TestInfoItem } from './TestInfoItem.ts'
 
+export interface ITestInfoCache {
+  readonly clear: () => void
+  readonly hasItems: () => boolean
+  readonly last: () => TestInfoItem
+  readonly push: (item: TestInfoItem) => void
+}
+
 let items: readonly TestInfoItem[] = []
 
 export const hasItems = (): boolean => {
