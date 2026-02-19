7734648557382b2e71563afb28aa410ee4edcebd
Le Vivilet <levivilet@pm.me>
2026-02-19 15:15:09 +0100

    feature: refactor build-static.ts to enhance file handling and update testWorkerUrl logic (#862)

--- packages/build/src/build-static.ts
+++ packages/build/src/build-static.ts
@@ -1,4 +1,4 @@
-import { cp } from 'node:fs/promises'
+import { cp, readFile, writeFile } from 'node:fs/promises'
 import { join } from 'node:path'
 import { pathToFileURL } from 'node:url'
 import { root } from './root.ts'
@@ -10,15 +10,30 @@ const sharedProcessUrl = pathToFileURL(sharedProcessPath).toString()
 const sharedProcess = await import(sharedProcessUrl)
 
 process.env.PATH_PREFIX = '/test-worker'
-const { commitHash }: { commitHash: string } = await sharedProcess.exportStatic({
+const { commitHash } = await sharedProcess.exportStatic({
   root,
   extensionPath: '',
   testPath: 'packages/e2e',
 })
 
-await cp(
-  join(root, '.tmp', 'dist', 'dist', 'testWorkerMain.js'),
-  join(root, 'dist', commitHash, 'packages', 'test-worker', 'dist', 'testWorkerMain.js'),
-)
+const rendererWorkerPath = join(root, 'dist', commitHash, 'packages', 'renderer-worker', 'dist', 'rendererWorkerMain.js')
+
+export const getRemoteUrl = (path: string): string => {
+  const url = pathToFileURL(path).toString().slice(8)
+  return `/remote/${url}`
+}
+
+const content = await readFile(rendererWorkerPath, 'utf8')
+const workerPath = join(root, '.tmp/dist/dist/testWorkerMain.js')
+const remoteUrl = getRemoteUrl(workerPath)
+
+const occurrence = `// const testWorkerUrl = \`\${assetDir}/packages/test-worker/dist/testWorkerMain.js\`
+const testWorkerUrl = \`${remoteUrl}\``
+const replacement = `const testWorkerUrl = \`\${assetDir}/packages/test-worker/dist/testWorkerMain.js\``
+if (!content.includes(occurrence)) {
+  throw new Error('occurrence not found')
+}
+const newContent = content.replace(occurrence, replacement)
+await writeFile(rendererWorkerPath, newContent)
 
 await cp(join(root, 'dist'), join(root, '.tmp', 'static'), { recursive: true })
