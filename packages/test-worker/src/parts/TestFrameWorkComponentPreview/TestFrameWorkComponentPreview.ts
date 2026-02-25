1fbee5dfc95ede4d838c2308a7595cd4faab763f
Le Vivilet <levivilet@pm.me>
2026-02-25 11:02:02 +0100

    feature: add mouse event handlers to TestFrameWorkComponentPreview (#880)

--- packages/test-worker/src/parts/TestFrameWorkComponentPreview/TestFrameWorkComponentPreview.ts
+++ packages/test-worker/src/parts/TestFrameWorkComponentPreview/TestFrameWorkComponentPreview.ts
@@ -11,6 +11,17 @@ export const handleClick = async (hdId: string): Promise<void> => {
 export const handleInput = async (hdId: string, value: string): Promise<void> => {
   await RendererWorker.invoke('Preview.handleInput', hdId, value)
 }
+export const handleMouseDown = async (hdId: string, clientX: number, clientY: number): Promise<void> => {
+  await RendererWorker.invoke('Preview.handleMouseDown', hdId, clientX, clientY)
+}
+
+export const handleMouseUp = async (hdId: string, value: string, clientX: number, clientY: number): Promise<void> => {
+  await RendererWorker.invoke('Preview.handleMouseUp', hdId, value, clientX, clientY)
+}
+
+export const handleMouseMove = async (hdId: string, value: string, clientX: number, clientY: number): Promise<void> => {
+  await RendererWorker.invoke('Preview.handleMouseMove', hdId, value, clientX, clientY)
+}
 
 export const handleKeyDown = async (hdId: string, key: string, code: string): Promise<void> => {
   await RendererWorker.invoke('Preview.handleKeyDown', hdId, key, code)
