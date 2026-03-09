537bbb5303ba3dbe277fb2f9866cb145443cfd96
Le Vivilet <levivilet@pm.me>
2026-03-09 16:00:43 +0100

    feature: add TestFrameWorkComponentChat with show, handleInput, and handleSubmit functions

---
+++ packages/test-worker/src/parts/TestFrameWorkComponentChat/TestFrameWorkComponentChat.ts
@@ -0,0 +1,14 @@
+import { RendererWorker } from '@lvce-editor/rpc-registry'
+
+export const show = async (): Promise<void> => {
+  await RendererWorker.invoke('Layout.showSecondarySideBar')
+  await RendererWorker.invoke('Chat.reset')
+}
+
+export const handleInput = async (text: string): Promise<void> => {
+  await RendererWorker.invoke('Chat.handleInput', 'composer', text, 'script')
+}
+
+export const handleSubmit = async (): Promise<void> => {
+  await RendererWorker.invoke('Chat.handleSubmit')
+}
