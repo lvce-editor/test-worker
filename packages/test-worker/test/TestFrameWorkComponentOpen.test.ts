8ba3e8c16d6e11bc03e0e1149896fa66ed9a5e86
Le Vivilet <levivilet@pm.me>
2026-03-18 17:19:31 +0100

    fix

--- packages/test-worker/test/TestFrameWorkComponentOpen.test.ts
+++ packages/test-worker/test/TestFrameWorkComponentOpen.test.ts
@@ -1,9 +1,9 @@
 import { expect, test } from '@jest/globals'
-import { RendererWorker } from '@lvce-editor/rpc-registry'
+import { OpenerWorker } from '@lvce-editor/rpc-registry'
 import * as Open from '../src/parts/TestFrameWorkComponentOpen/TestFrameworkComponentOpen.ts'
 
 test('enableMemoryOpener', async () => {
-  using mockRpc = RendererWorker.registerMockRpc({
+  using mockRpc = OpenerWorker.registerMockRpc({
     'Open.enableMemoryOpener'() {
       return undefined
     },
@@ -13,7 +13,7 @@ test('enableMemoryOpener', async () => {
 })
 
 test('disableMemoryOpener', async () => {
-  using mockRpc = RendererWorker.registerMockRpc({
+  using mockRpc = OpenerWorker.registerMockRpc({
     'Open.disableMemoryOpener'() {
       return undefined
     },
@@ -23,45 +23,45 @@ test('disableMemoryOpener', async () => {
 })
 
 test('shouldHaveUrl - string success', async () => {
-  using mockRpc = RendererWorker.registerMockRpc({
-    'Open.readMemoryUrl'() {
+  using mockRpc = OpenerWorker.registerMockRpc({
+    'Open.readOpenedUrl'() {
       return 'https://example.com'
     },
   })
   await Open.shouldHaveUrl('https://example.com')
-  expect(mockRpc.invocations).toEqual([['Open.readMemoryUrl']])
+  expect(mockRpc.invocations).toEqual([['Open.readOpenedUrl']])
 })
 
 test('shouldHaveUrl - string error', async () => {
-  using mockRpc = RendererWorker.registerMockRpc({
-    'Open.readMemoryUrl'() {
+  using mockRpc = OpenerWorker.registerMockRpc({
+    'Open.readOpenedUrl'() {
       return 'https://wrong.example.com'
     },
   })
   await expect(Open.shouldHaveUrl('https://example.com')).rejects.toThrow(
     'expected opened url to be "https://example.com" but was "https://wrong.example.com"',
   )
-  expect(mockRpc.invocations).toEqual([['Open.readMemoryUrl']])
+  expect(mockRpc.invocations).toEqual([['Open.readOpenedUrl']])
 })
 
 test('shouldHaveUrl - regex success', async () => {
-  using mockRpc = RendererWorker.registerMockRpc({
-    'Open.readMemoryUrl'() {
+  using mockRpc = OpenerWorker.registerMockRpc({
+    'Open.readOpenedUrl'() {
       return 'https://example.com/path/123'
     },
   })
   await Open.shouldHaveUrl(/https:\/\/example\.com\/path\/\d+/)
-  expect(mockRpc.invocations).toEqual([['Open.readMemoryUrl']])
+  expect(mockRpc.invocations).toEqual([['Open.readOpenedUrl']])
 })
 
 test('shouldHaveUrl - regex error', async () => {
-  using mockRpc = RendererWorker.registerMockRpc({
-    'Open.readMemoryUrl'() {
+  using mockRpc = OpenerWorker.registerMockRpc({
+    'Open.readOpenedUrl'() {
       return 'https://example.com/path/no-number'
     },
   })
   await expect(Open.shouldHaveUrl(/https:\/\/example\.com\/path\/\d+/)).rejects.toThrow(
     'expected opened url to be "/https:\\/\\/example\\.com\\/path\\/\\d+/" but was "https://example.com/path/no-number"',
   )
-  expect(mockRpc.invocations).toEqual([['Open.readMemoryUrl']])
+  expect(mockRpc.invocations).toEqual([['Open.readOpenedUrl']])
 })
