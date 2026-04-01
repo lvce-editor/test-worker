9036766c1482fb9883a17bf185116ef55c482161
Le Vivilet <levivilet@pm.me>
2026-04-01 15:42:35 +0200

    fix

--- packages/test-worker/test/TestFrameWorkComponentWebView.test.ts
+++ packages/test-worker/test/TestFrameWorkComponentWebView.test.ts
@@ -1,9 +1,10 @@
 import { expect, jest, test } from '@jest/globals'
-import { MockRpc } from '@lvce-editor/rpc'
+import { createMockRpc } from '@lvce-editor/rpc'
 import * as WebViewState from '../src/parts/WebViewState/WebViewState.ts'
 
 const invoke: jest.Mock = jest.fn()
-const mockRpc = MockRpc.create({ commandMap: {}, invoke })
+const mockRpc = createMockRpc({ commandMap: {} })
+mockRpc.invoke = invoke
 
 jest.unstable_mockModule('../src/parts/CreatePortRpc/CreatePortRpc.ts', () => ({
   // @ts-ignore
