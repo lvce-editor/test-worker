cfc48f28a4c9eb7673f1e87c1fc628bbe8fa95e0
Le Vivilet <levivilet@pm.me>
2026-04-01 15:54:09 +0200

    feature: add tsconfig plugin to ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,13 @@
 import config from '@lvce-editor/eslint-config'
 import actions from '@lvce-editor/eslint-plugin-github-actions'
 import regex from '@lvce-editor/eslint-plugin-regex'
+import tsconfig from '@lvce-editor/eslint-plugin-tsconfig'
 
 export default [
   ...config,
   ...actions,
   ...regex,
+  ...tsconfig,
   {
     ignores: ['src/testWorkerMain.ts'],
   },
