d1fb4a0ba90770e6dca5f6f13dabef524f478df1
Le Vivilet <levivilet@pm.me>
2026-04-12 08:11:07 +0200

    feature: update getLocatorInvoke and performAction to use ILocator type for improved type safety

--- packages/test-worker/src/parts/PerformAction/PerformAction.ts
+++ packages/test-worker/src/parts/PerformAction/PerformAction.ts
@@ -1,6 +1,7 @@
+import type { ILocator } from '../ILocator/ILocator.ts'
 import { getLocatorInvoke } from '../GetLocatorInvoke/GetLocatorInvoke.ts'
 
-export const performAction = async (locator: any, action: string, options: any): Promise<any> => {
+export const performAction = async (locator: ILocator, action: string, options: any): Promise<any> => {
   const invoke = getLocatorInvoke(locator)
   return invoke('TestFrameWork.performAction', locator, action, options)
 }
