8c9c426efbd201166fa820d5056b25f8b14d7a76
Le Vivilet <levivilet@pm.me>
2026-04-12 08:12:59 +0200

    feature: update getLocatorInvoke and performAction to use ILocator type for improved type safety (#974)

--- packages/test-worker/src/parts/GetLocatorInvoke/GetLocatorInvoke.ts
+++ packages/test-worker/src/parts/GetLocatorInvoke/GetLocatorInvoke.ts
@@ -1,12 +1,15 @@
 import { RendererWorker } from '@lvce-editor/rpc-registry'
+import type { ILocator } from '../ILocator/ILocator.ts'
 import * as WebViewState from '../WebViewState/WebViewState.ts'
 
 interface Invoke {
   (method: string, ...params: readonly any[]): Promise<any>
 }
 
-export const getLocatorInvoke = (locator: any): Invoke => {
+export const getLocatorInvoke = (locator: ILocator): Invoke => {
+  // @ts-ignore
   if (locator.webViewId) {
+    // @ts-ignore
     const module = WebViewState.get(locator.webViewId)
     return module.invoke
   }
