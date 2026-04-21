b594b1b3a0a352d0181ccd61da971c4a0df352e0
Le Vivilet <levivilet@pm.me>
2026-04-21 16:33:41 +0200

    feature: add content validation functions for DiffView component (#993)

--- packages/test-worker/src/parts/TestFrameWorkComponentDiffView/TestFrameWorkComponentDiffView.ts
+++ packages/test-worker/src/parts/TestFrameWorkComponentDiffView/TestFrameWorkComponentDiffView.ts
@@ -1,5 +1,16 @@
+import { createLocator } from '../CreateLocator/CreateLocator.ts'
+import { expect } from '../Expect/Expect.ts'
 import * as Main from '../TestFrameWorkComponentMain/TestFrameWorkComponentMain.ts'
 
 export const open = async (leftUri: string, rightUri: string): Promise<void> => {
   await Main.openUri(`diff://${leftUri}<->${rightUri}`)
+}
+
+export const shouldHaveContentLeft = async (expectedContent: string): Promise<void> => {
+  const contentLeft = createLocator('.DiffEditorContentLeft')
+  await expect(contentLeft).toHaveText(expectedContent)
+}
+export const shouldHaveContentRight = async (expectedContent: string): Promise<void> => {
+  const contentRight = createLocator('.DiffEditorContentRight')
+  await expect(contentRight).toHaveText(expectedContent)
 }
