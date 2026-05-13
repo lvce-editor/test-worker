9580acbe8ef3c33f6f4f3e584ec05f22dd9f136a
Le Vivilet <levivilet@pm.me>
2026-05-13 10:40:22 +0000

    feature: add hideSideBar and showSecondarySideBar functions to manage sidebar visibility

--- packages/test-worker/src/parts/TestFrameWorkComponentLayout/TestFrameWorkComponentLayout.ts
+++ packages/test-worker/src/parts/TestFrameWorkComponentLayout/TestFrameWorkComponentLayout.ts
@@ -5,6 +5,18 @@ export const showSideBar = async (): Promise<void> => {
   await Command.execute('Layout.showSideBar')
 }
 
+export const hideSideBar = async (): Promise<void> => {
+  await Command.execute('Layout.hideSideBar')
+}
+
+export const showSecondarySideBar = async (): Promise<void> => {
+  await Command.execute('Layout.showSecondarySideBar')
+}
+
+export const hideSecondarySideBar = async (): Promise<void> => {
+  await Command.execute('Layout.hideSecondarySideBar')
+}
+
 export const handleWorkspaceRefresh = async (): Promise<void> => {
   await RendererWorker.invoke('Layout.handleWorkspaceRefresh')
 }
