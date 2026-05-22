91e37c7dababf11f5bd066bac8b97623f3433f4e
Le Vivilet <levivilet@pm.me>
2026-05-22 14:51:54 +0200

    feature: implement tryAutoFix functionality and add corresponding tests (#1063)

--- packages/test-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/test-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,9 +1,11 @@
 import { handleFileWatcherEvent } from '../HandleFileWatcherEvent/HandleFileWatcherEvent.ts'
 import * as Test from '../Test/Test.ts'
 import * as TestFrameWorkComponentDialog from '../TestFrameWorkComponentDialog/TestFrameWorkComponentDialog.ts'
+import * as TryAutoFix from '../TryAutoFix/TryAutoFix.ts'
 
 export const commandMap = {
   'FileWatcher.handleEvent': handleFileWatcherEvent,
   'Test.execute': Test.execute,
   'Test.executeMock': TestFrameWorkComponentDialog.executeMock,
+  'Test.tryAutoFix': TryAutoFix.tryAutoFix,
 }
