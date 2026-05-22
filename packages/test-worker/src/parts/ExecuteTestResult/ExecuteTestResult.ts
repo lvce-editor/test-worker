67387d4f3e927b6d67a819f5b954ab875a553508
Le Vivilet <levivilet@pm.me>
2026-05-22 14:38:15 +0200

    feature: add autoFixError property to ExecuteTestResult interface and create OverlayAction interface

--- packages/test-worker/src/parts/ExecuteTestResult/ExecuteTestResult.ts
+++ packages/test-worker/src/parts/ExecuteTestResult/ExecuteTestResult.ts
@@ -1,10 +1,15 @@
+import type { AutoFixError } from '../AutoFixError/AutoFixError.ts'
+import type { OverlayAction } from '../OverlayAction/OverlayAction.ts'
+
 export interface ExecuteTestResult {
+  readonly autoFixError?: AutoFixError
   readonly background: string
   readonly duration: number
   readonly end: number
   readonly error: any
   readonly formattedDuration: string
   readonly name: string
+  readonly overlayActions?: readonly OverlayAction[]
   readonly start: number
   readonly text: string
   readonly type: string
---
+++ packages/test-worker/src/parts/OverlayAction/OverlayAction.ts
@@ -0,0 +1,5 @@
+export interface OverlayAction {
+  readonly command: string
+  readonly id: string
+  readonly label: string
+}
