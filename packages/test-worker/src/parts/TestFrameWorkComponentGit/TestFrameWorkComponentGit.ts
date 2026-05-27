45360f059d9e102046f3a447fd53574d4e8a40ab
Le Vivilet <levivilet@pm.me>
2026-05-27 12:17:12 +0200

    feature: update Git command invocations to use ExtensionHost.executeCommand (#1090)
    * feature: update Git command invocations to use ExtensionHost.executeCommand
    
    * fix
    
    * fix

--- packages/test-worker/src/parts/TestFrameWorkComponentGit/TestFrameWorkComponentGit.ts
+++ packages/test-worker/src/parts/TestFrameWorkComponentGit/TestFrameWorkComponentGit.ts
@@ -1,51 +1,52 @@
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 
 export const init = async (): Promise<void> => {
-  await RendererWorker.invoke('Git.init')
+  // await RendererWorker.invoke('ExtensionHostManagement.activateByEvent', 'onSourceControl:')
+  await RendererWorker.invoke('ExtensionHost.executeCommand', 'git.init')
 }
 
 export const clone = async (repositoryUrl: string, cwd: string): Promise<void> => {
-  await RendererWorker.invoke('Git.clone', repositoryUrl, cwd)
+  await RendererWorker.invoke('ExtensionHost.executeCommand', 'Git.clone', repositoryUrl, cwd)
 }
 
 export const add = async (pathSpec: string): Promise<void> => {
-  await RendererWorker.invoke('Git.add', pathSpec)
+  await RendererWorker.invoke('ExtensionHost.executeCommand', 'Git.add', pathSpec)
 }
 
 export const commit = async (message: string): Promise<void> => {
-  await RendererWorker.invoke('Git.commit', message)
+  await RendererWorker.invoke('ExtensionHost.executeCommand', 'Git.commit', message)
 }
 
 export const push = async (remote: string, branch: string): Promise<void> => {
-  await RendererWorker.invoke('Git.push', remote, branch)
+  await RendererWorker.invoke('ExtensionHost.executeCommand', 'Git.push', remote, branch)
 }
 
 export const pull = async (remote: string, branch: string): Promise<void> => {
-  await RendererWorker.invoke('Git.pull', remote, branch)
+  await RendererWorker.invoke('ExtensionHost.executeCommand', 'Git.pull', remote, branch)
 }
 
 export const fetch = async (remote: string): Promise<void> => {
-  await RendererWorker.invoke('Git.fetch', remote)
+  await RendererWorker.invoke('ExtensionHost.executeCommand', 'Git.fetch', remote)
 }
 
 export const checkout = async (ref: string): Promise<void> => {
-  await RendererWorker.invoke('Git.checkout', ref)
+  await RendererWorker.invoke('ExtensionHost.executeCommand', 'Git.checkout', ref)
 }
 
 export const branch = async (name: string): Promise<void> => {
-  await RendererWorker.invoke('Git.branch', name)
+  await RendererWorker.invoke('ExtensionHost.executeCommand', 'Git.branch', name)
 }
 
 export const status = async (): Promise<unknown> => {
-  return RendererWorker.invoke('Git.status')
+  return RendererWorker.invoke('ExtensionHost.executeCommand', 'Git.status')
 }
 
 export const addRemote = async (name: string, remoteUrl: string): Promise<void> => {
-  await RendererWorker.invoke('Git.addRemote', name, remoteUrl)
+  await RendererWorker.invoke('ExtensionHost.executeCommand', 'Git.addRemote', name, remoteUrl)
 }
 
 export const setConfig = async (key: string, value: string): Promise<void> => {
-  await RendererWorker.invoke('Git.setConfig', key, value)
+  await RendererWorker.invoke('ExtensionHost.executeCommand', 'Git.setConfig', key, value)
 }
 
 export const shouldHaveInvocations = async (invocations: readonly unknown[][]): Promise<void> => {
