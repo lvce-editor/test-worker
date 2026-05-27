0c5f2845b054db5619c2264c6fe3c4895003218a
Le Vivilet <levivilet@pm.me>
2026-05-27 12:09:49 +0200

    fix

--- packages/test-worker/test/TestFrameWorkComponentGit.test.ts
+++ packages/test-worker/test/TestFrameWorkComponentGit.test.ts
@@ -4,134 +4,136 @@ import * as Git from '../src/parts/TestFrameWorkComponentGit/TestFrameWorkCompon
 
 test('init', async () => {
   using mockRpc = RendererWorker.registerMockRpc({
-    'Git.init'() {
+    'ExtensionHost.executeCommand'() {
       return undefined
     },
   })
 
   await Git.init()
-  expect(mockRpc.invocations).toEqual([['Git.init']])
+  expect(mockRpc.invocations).toEqual([['ExtensionHost.executeCommand', 'git.init']])
 })
 
 test('clone', async () => {
   using mockRpc = RendererWorker.registerMockRpc({
-    'Git.clone'() {
+    'ExtensionHost.executeCommand'() {
       return undefined
     },
   })
 
   await Git.clone('https://github.com/lvce-editor/test-worker.git', '/workspace')
-  expect(mockRpc.invocations).toEqual([['Git.clone', 'https://github.com/lvce-editor/test-worker.git', '/workspace']])
+  expect(mockRpc.invocations).toEqual([['ExtensionHost.executeCommand', 'Git.clone', 'https://github.com/lvce-editor/test-worker.git', '/workspace']])
 })
 
 test('add', async () => {
   using mockRpc = RendererWorker.registerMockRpc({
-    'Git.add'() {
+    'ExtensionHost.executeCommand'() {
       return undefined
     },
   })
 
   await Git.add('.')
-  expect(mockRpc.invocations).toEqual([['Git.add', '.']])
+  expect(mockRpc.invocations).toEqual([['ExtensionHost.executeCommand', 'Git.add', '.']])
 })
 
 test('commit', async () => {
   using mockRpc = RendererWorker.registerMockRpc({
-    'Git.commit'() {
+    'ExtensionHost.executeCommand'() {
       return undefined
     },
   })
 
   await Git.commit('feat: initial commit')
-  expect(mockRpc.invocations).toEqual([['Git.commit', 'feat: initial commit']])
+  expect(mockRpc.invocations).toEqual([['ExtensionHost.executeCommand', 'Git.commit', 'feat: initial commit']])
 })
 
 test('push', async () => {
   using mockRpc = RendererWorker.registerMockRpc({
-    'Git.push'() {
+    'ExtensionHost.executeCommand'() {
       return undefined
     },
   })
 
   await Git.push('origin', 'main')
-  expect(mockRpc.invocations).toEqual([['Git.push', 'origin', 'main']])
+  expect(mockRpc.invocations).toEqual([['ExtensionHost.executeCommand', 'Git.push', 'origin', 'main']])
 })
 
 test('pull', async () => {
   using mockRpc = RendererWorker.registerMockRpc({
-    'Git.pull'() {
+    'ExtensionHost.executeCommand'() {
       return undefined
     },
   })
 
   await Git.pull('origin', 'main')
-  expect(mockRpc.invocations).toEqual([['Git.pull', 'origin', 'main']])
+  expect(mockRpc.invocations).toEqual([['ExtensionHost.executeCommand', 'Git.pull', 'origin', 'main']])
 })
 
 test('fetch', async () => {
   using mockRpc = RendererWorker.registerMockRpc({
-    'Git.fetch'() {
+    'ExtensionHost.executeCommand'() {
       return undefined
     },
   })
 
   await Git.fetch('origin')
-  expect(mockRpc.invocations).toEqual([['Git.fetch', 'origin']])
+  expect(mockRpc.invocations).toEqual([['ExtensionHost.executeCommand', 'Git.fetch', 'origin']])
 })
 
 test('checkout', async () => {
   using mockRpc = RendererWorker.registerMockRpc({
-    'Git.checkout'() {
+    'ExtensionHost.executeCommand'() {
       return undefined
     },
   })
 
   await Git.checkout('feature/test')
-  expect(mockRpc.invocations).toEqual([['Git.checkout', 'feature/test']])
+  expect(mockRpc.invocations).toEqual([['ExtensionHost.executeCommand', 'Git.checkout', 'feature/test']])
 })
 
 test('branch', async () => {
   using mockRpc = RendererWorker.registerMockRpc({
-    'Git.branch'() {
+    'ExtensionHost.executeCommand'() {
       return undefined
     },
   })
 
   await Git.branch('feature/test')
-  expect(mockRpc.invocations).toEqual([['Git.branch', 'feature/test']])
+  expect(mockRpc.invocations).toEqual([['ExtensionHost.executeCommand', 'Git.branch', 'feature/test']])
 })
 
 test('status', async () => {
   const gitStatus = { branch: 'main', staged: ['a.ts'], unstaged: ['b.ts'] }
   using mockRpc = RendererWorker.registerMockRpc({
-    'Git.status'() {
+    'ExtensionHost.executeCommand'() {
       return gitStatus
     },
   })
 
   const result = await Git.status()
   expect(result).toBe(gitStatus)
-  expect(mockRpc.invocations).toEqual([['Git.status']])
+  expect(mockRpc.invocations).toEqual([['ExtensionHost.executeCommand', 'Git.status']])
 })
 
 test('addRemote', async () => {
   using mockRpc = RendererWorker.registerMockRpc({
-    'Git.addRemote'() {
+    'ExtensionHost.executeCommand'() {
       return undefined
     },
   })
 
   await Git.addRemote('origin', 'https://github.com/lvce-editor/test-worker.git')
-  expect(mockRpc.invocations).toEqual([['Git.addRemote', 'origin', 'https://github.com/lvce-editor/test-worker.git']])
+  expect(mockRpc.invocations).toEqual([
+    ['ExtensionHost.executeCommand', 'Git.addRemote', 'origin', 'https://github.com/lvce-editor/test-worker.git'],
+  ])
 })
 
 test('setConfig', async () => {
   using mockRpc = RendererWorker.registerMockRpc({
-    'Git.setConfig'() {
+    'ExtensionHost.executeCommand'() {
       return undefined
     },
   })
 
   await Git.setConfig('user.name', 'Lvce Editor')
-  expect(mockRpc.invocations).toEqual([['Git.setConfig', 'user.name', 'Lvce Editor']])
+  expect(mockRpc.invocations).toEqual([['ExtensionHost.executeCommand', 'Git.setConfig', 'user.name', 'Lvce Editor']])
 })
