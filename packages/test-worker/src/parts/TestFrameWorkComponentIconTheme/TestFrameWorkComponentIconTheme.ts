f395cc6c456403a501986e0abc79df8876c242bb
Le Vivilet <levivilet@pm.me>
2026-05-28 16:13:10 +0200

    rename

--- packages/test-worker/src/parts/TestFrameWorkComponentIconTheme/TestFrameWorkComponentIconTheme.ts
+++ packages/test-worker/src/parts/TestFrameWorkComponentIconTheme/TestFrameWorkComponentIconTheme.ts
@@ -1,5 +1,5 @@
-import { RendererWorker as Rpc } from '@lvce-editor/rpc-registry'
+import { RendererWorker } from '@lvce-editor/rpc-registry'
 
 export const setIconTheme = async (id: string): Promise<void> => {
-  await Rpc.invoke('IconTheme.setIconTheme', id)
+  await RendererWorker.invoke('IconTheme.setIconTheme', id)
 }
