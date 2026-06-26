2f4c79dadd8c06a4f4a52e931856b54ac03f25a9
Le Vivilet <levivilet@pm.me>
2026-06-26 16:40:59 +0200

    feature: use extension management worker for executing extension command

--- packages/test-worker/src/parts/TestFrameWorkComponentCommand/TestFrameWorkComponentCommand.ts
+++ packages/test-worker/src/parts/TestFrameWorkComponentCommand/TestFrameWorkComponentCommand.ts
@@ -1,10 +1,15 @@
-import { RendererWorker } from '@lvce-editor/rpc-registry'
+import { ExtensionManagementWorker, RendererWorker } from '@lvce-editor/rpc-registry'
 
 export const execute = async (id: string, ...args: readonly any[]): Promise<any> => {
   return RendererWorker.invoke(id, ...args)
 }
 
 export const executeExtensionCommand = async (commandId: string, ...args: readonly any[]): Promise<unknown> => {
-  // TODO maybe ask extension-management-worker instead
-  return RendererWorker.invoke('ExtensionHost.executeCommand', commandId, ...args)
+  try {
+    return await ExtensionManagementWorker.invoke('Extensions.executeExtensionCommand', commandId, ...args)
+  } catch {
+    // legacy
+    // TODO maybe ask extension-management-worker instead
+    return RendererWorker.invoke('ExtensionHost.executeCommand', commandId, ...args)
+  }
 }
