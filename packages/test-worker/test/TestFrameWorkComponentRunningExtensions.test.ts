f193e22491c612c53c293e40d4665ccaeac490f9
Le Vivilet <levivilet@pm.me>
2026-07-13 13:32:55 +0000

    feature: add running extensions page object

---
+++ packages/test-worker/test/TestFrameWorkComponentRunningExtensions.test.ts
@@ -0,0 +1,32 @@
+import { expect, test } from '@jest/globals'
+import { RendererWorker } from '@lvce-editor/rpc-registry'
+import * as RunningExtensions from '../src/parts/TestFrameWorkComponentRunningExtensions/TestFrameWorkComponentRunningExtensions.ts'
+
+test('commands', async () => {
+  using mockRpc = RendererWorker.registerMockRpc({
+    'Main.openUri'() {},
+    'RunningExtensions.copyId'() {},
+    'RunningExtensions.disable'() {},
+    'RunningExtensions.disableWorkspace'() {},
+    'RunningExtensions.handleContextMenu'() {},
+    'RunningExtensions.startProfile'() {},
+  })
+
+  await RunningExtensions.show()
+  await RunningExtensions.handleContextMenu(1, 10, 20)
+  await RunningExtensions.handleContextMenu(2)
+  await RunningExtensions.copyId(3)
+  await RunningExtensions.disable(4)
+  await RunningExtensions.disableWorkspace(5)
+  await RunningExtensions.startProfile()
+
+  expect(mockRpc.invocations).toEqual([
+    ['Main.openUri', 'running-extensions:///1'],
+    ['RunningExtensions.handleContextMenu', 1, 10, 20],
+    ['RunningExtensions.handleContextMenu', 2, 0, 0],
+    ['RunningExtensions.copyId', 3],
+    ['RunningExtensions.disable', 4],
+    ['RunningExtensions.disableWorkspace', 5],
+    ['RunningExtensions.startProfile'],
+  ])
+})
