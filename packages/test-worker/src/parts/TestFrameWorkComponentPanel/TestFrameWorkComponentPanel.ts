9b129dd2cbe6c3611db369c9df9cf3f6aca790bc
Le Vivilet <levivilet@pm.me>
2026-07-25 20:16:31 +0000

    feature: add panel test helpers

--- packages/test-worker/src/parts/TestFrameWorkComponentPanel/TestFrameWorkComponentPanel.ts
+++ packages/test-worker/src/parts/TestFrameWorkComponentPanel/TestFrameWorkComponentPanel.ts
@@ -1,5 +1,13 @@
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 
+export const close = async (): Promise<void> => {
+  await RendererWorker.invoke('Panel.handleClickClose')
+}
+
+export const hide = async (): Promise<void> => {
+  await RendererWorker.invoke('Layout.hidePanel')
+}
+
 export const open = async (id: string): Promise<void> => {
   await RendererWorker.invoke('Layout.showPanel', id)
 }
@@ -10,10 +18,14 @@ export const openProblems = async (): Promise<void> => {
   await RendererWorker.invoke('Panel.selectIndex', 0)
 }
 
+export const select = async (name: string): Promise<void> => {
+  await RendererWorker.invoke('Panel.selectName', name)
+}
+
 export const maximize = async (): Promise<void> => {
-  await RendererWorker.invoke('Panel.maximize')
+  await RendererWorker.invoke('Layout.maximizePanel')
 }
 
 export const unmaximize = async (): Promise<void> => {
-  await RendererWorker.invoke('Panel.unmaximize')
+  await RendererWorker.invoke('Layout.unmaximizePanel')
 }
