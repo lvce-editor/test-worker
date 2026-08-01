229cc332b7143a9139c26eaeb717f3f16deaee4c
Le Vivilet <levivilet@pm.me>
2026-08-01 01:25:28 -0700

    feature: add extension search page object commands (#1260)
    * feature: add extension search page object commands
    
    * fix: make input source declaration self-contained

--- packages/test-worker/src/parts/TestFrameWorkComponentContextMenu/TestFrameWorkComponentContextMenu.ts
+++ packages/test-worker/src/parts/TestFrameWorkComponentContextMenu/TestFrameWorkComponentContextMenu.ts
@@ -1,5 +1,9 @@
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 
+export const selectIndex = async (level: number, index: number): Promise<void> => {
+  await RendererWorker.invoke('Menu.selectIndex', level, index)
+}
+
 export const selectItem = async (text: string): Promise<void> => {
   await RendererWorker.invoke('Menu.selectItem', text)
 }
--- packages/test-worker/test/TestFrameWorkComponentContextMenu.test.ts
+++ packages/test-worker/test/TestFrameWorkComponentContextMenu.test.ts
@@ -2,6 +2,16 @@ import { expect, test } from '@jest/globals'
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 import * as ContextMenu from '../src/parts/TestFrameWorkComponentContextMenu/TestFrameWorkComponentContextMenu.ts'
 
+test('selectIndex', async () => {
+  using mockRpc = RendererWorker.registerMockRpc({
+    'Menu.selectIndex'() {
+      return undefined
+    },
+  })
+  await ContextMenu.selectIndex(0, 2)
+  expect(mockRpc.invocations).toEqual([['Menu.selectIndex', 0, 2]])
+})
+
 test('selectItem', async () => {
   using mockRpc = RendererWorker.registerMockRpc({
     'Menu.selectItem'() {
