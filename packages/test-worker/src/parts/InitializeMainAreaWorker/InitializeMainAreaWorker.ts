6af74b8b86ca3dea8822f1260f748cdb37ca7330
Le Vivilet <levivilet@pm.me>
2026-08-17 09:01:54 +0000

    fix: decouple main area initialization

--- packages/test-worker/src/parts/InitializeMainAreaWorker/InitializeMainAreaWorker.ts
+++ packages/test-worker/src/parts/InitializeMainAreaWorker/InitializeMainAreaWorker.ts
@@ -2,7 +2,12 @@ import { LazyTransferMessagePortRpcParent } from '@lvce-editor/rpc'
 import { MainAreaWorker, RendererWorker, RpcId } from '@lvce-editor/rpc-registry'
 
 const send = async (port: MessagePort): Promise<void> => {
-  await RendererWorker.sendMessagePortToMainAreaWorker(port, RpcId.TestWorker)
+  await RendererWorker.invokeAndTransfer(
+    'SendMessagePortToExtensionHostWorker.sendMessagePortToMainAreaWorker',
+    port,
+    'MainArea.handleTestWorkerMessagePort',
+    RpcId.TestWorker,
+  )
 }
 
 export const initializeMainAreaWorker = async (): Promise<void> => {
