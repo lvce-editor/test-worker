7e1113ac5c02a04f0bfe29f63407733dc015c378
Le Vivilet <levivilet@pm.me>
2026-09-12 15:03:42 +0200

    feature: add status bar page object functions (#1324)
    * feature: add status bar page object functions
    
    * fix: keep status bar helpers within worker memory budget
    
    * test: cover every renderer-only command route
    
    * fix: sort renderer command cases

--- packages/test-worker/src/parts/TestFrameWorkComponentStatusBar/TestFrameWorkComponentStatusBar.ts
+++ packages/test-worker/src/parts/TestFrameWorkComponentStatusBar/TestFrameWorkComponentStatusBar.ts
@@ -3,3 +3,11 @@ import * as DirectViewWorker from '../DirectViewWorker/DirectViewWorker.ts'
 export const update = async (): Promise<void> => {
   await DirectViewWorker.invoke('StatusBar', 'StatusBar.updateStatusBarItems')
 }
+
+export const handleContextMenu = async (button: number, x: number, y: number): Promise<void> => {
+  await DirectViewWorker.invoke('StatusBar', 'StatusBar.handleContextMenu', button, x, y)
+}
+
+export const click = async (name: string): Promise<void> => {
+  await DirectViewWorker.invoke('StatusBar', 'StatusBar.handleClick', name)
+}
