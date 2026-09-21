e0dd028c7790fb16bbf55df66e483d23bfd50b6c
levivilet <72156503+levivilet@users.noreply.github.com>
2026-09-21 06:56:29 +0000

    fix: satisfy markdown preview lint

--- packages/test-worker/src/parts/TestFrameWorkComponentMarkdownPreview/TestFrameWorkComponentMarkdownPreview.ts
+++ packages/test-worker/src/parts/TestFrameWorkComponentMarkdownPreview/TestFrameWorkComponentMarkdownPreview.ts
@@ -1,8 +1,9 @@
+import type { ILocatorExternal } from '../ILocatorExternal/ILocatorExternal.ts'
 import { createLocator } from '../CreateLocator/CreateLocator.ts'
 import { expect } from '../Expect/Expect.ts'
 import * as Command from '../TestFrameWorkComponentCommand/TestFrameWorkComponentCommand.ts'
 
-const webView = () => {
+const webView = (): ILocatorExternal => {
   return createLocator('.WebViewIframe')
 }
 
